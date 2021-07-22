b3f5fce9a67218e685c5a7a5311160650b2e0ebe
Tim Regan <timregan@microsoft.com>
2021-07-22 09:20:24 +0100

    --azureml flag

--- src/health/azure/examples/elevate_this.py
+++ src/health/azure/examples/elevate_this.py
@@ -19,12 +19,14 @@ def main() -> None:
     Write out the given message, in an AzureML 'experiment' if required.
     """
     parser = ArgumentParser()
+    parser.add_argument("--azureml", type=bool, required=False, help="Flag to say whether to elevate script to AzureML")
     parser.add_argument("-m", "--message", type=str, required=True, help="The message to print out")
     parser.add_argument("-w", "--workspace_config_path", type=str, required=True, help="AzureML workspace config file")
     parser.add_argument("-c", "--compute_cluster_name", type=str, required=True, help="AzureML compute cluster to use")
     parser.add_argument("-e", "--conda_env", type=str, required=True, help="Conda environment YAML file")
     args = parser.parse_args()
 
+    # N.B. submit_to_azure_if_needed reads the --azureml flag from sys.argv and so it is not passed in as a parameter.
     submit_to_azure_if_needed(
         None,
         args.workspace_config_path,
