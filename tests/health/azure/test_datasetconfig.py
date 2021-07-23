2828265cbe41c441fc5a50212badd90b97c16379
Anton Schwaighofer <antonsc@microsoft.com>
2021-07-23 12:23:54 +0100

    formatting

--- tests/health/azure/test_datasetconfig.py
+++ tests/health/azure/test_datasetconfig.py
@@ -7,4 +7,3 @@ def test_datasetconfig_init() -> None:
     with pytest.raises(ValueError) as ex:
         DatasetConfig(name=" ")
     assert "name of the dataset must be a non-empty string" in str(ex)
-
