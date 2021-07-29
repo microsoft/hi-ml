a876b8413eaae68765ae55aad9f30ec50b8c87a4
Jonathan Tripp <v-jontri@microsoft.com>
2021-07-29 10:00:29 +0000

    Don't pass extra args to elevate_this

--- src/health/azure/examples/elevate_this.py
+++ src/health/azure/examples/elevate_this.py
@@ -34,7 +34,7 @@ def main() -> None:
 
     parser = ArgumentParser()
     parser.add_argument("-m", "--message", type=str, required=True, help="The message to print out")
-    args, _ = parser.parse_known_args()
+    args = parser.parse_args()
 
     print(f"The message was: {args.message}")
 
