a756926262574ac96f017b29d3e5005f2b1620fb
Tim Regan <timregan@microsoft.com>
2021-07-30 13:30:40 +0100

    importing some InnerEye tests

--- testhiml/health/azure/test_azure_util.py
+++ testhiml/health/azure/test_azure_util.py
@@ -123,10 +123,32 @@ def test_to_azure_friendly_string() -> None:
 
 def test_split_recovery_id() -> None:
     """
-    Other tests test most of the functionality provided by split_recovery_id, but they do not test the exception when
-    there are too many splits
+    Other tests test the main branch of split_recovery_id, but they do not test the exception when there are too many
+    splits, nor the regex branch
     """
     with pytest.raises(ValueError) as e:
         id = util.EXPERIMENT_RUN_SEPARATOR.join([str(i) for i in range(3)])
         util.split_recovery_id(id)
     assert str(e.value) == f"recovery_id must be in the format: 'experiment_name:run_id', but got: {id}"
+    with pytest.raises(ValueError) as e:
+        id = 
+        util.split_recovery_id(id)
+    assert str(e.value) == f"The recovery ID was not in the expected format: {id}"
+
+
+@pytest.mark.parametrize(["id", "expected1", "expected2"],
+                         [("foo:bar", "foo", "bar"),
+                          ("foo:bar_ab_cd", "foo", "bar_ab_cd"),
+                          ("a_b_c_00_123", "a_b_c", "a_b_c_00_123"),
+                          ("baz_00_123", "baz", "baz_00_123"),
+                          ("foo_bar_abc_123_456", "foo_bar_abc", "foo_bar_abc_123_456"),
+                          # This is the run ID of a hyperdrive parent run. It only has one numeric part at the end
+                          ("foo_bar_123", "foo_bar", "foo_bar_123"),
+                          # This is a hyperdrive child run
+                          ("foo_bar_123_3", "foo_bar", "foo_bar_123_3"),
+                          ])
+def test_split_recovery_id(id: str, expected1: str, expected2: str) -> None:
+    """
+    Check that run recovery ids are correctly parsed into experiment and run id.
+    """
+    assert util.split_recovery_id(id) == (expected1, expected2)
