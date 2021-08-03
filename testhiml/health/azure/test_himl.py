d1b0d0b8c9588192f1f27e70da438f01f25bc226
Tim Regan <timregan@microsoft.com>
2021-08-03 07:57:57 +0000

    Dropping unneeded mock experiment

--- testhiml/health/azure/test_himl.py
+++ testhiml/health/azure/test_himl.py
@@ -72,18 +72,16 @@ def test_submit_to_azure_if_needed_returns_immediately() -> None:
         assert not result.is_running_in_azure
 
 
-@patch("health.azure.himl.Experiment")
 @patch("health.azure.himl.Run")
-def test_write_run_recovery_file(mock_run: mock.MagicMock, mock_experiment: mock.MagicMock) -> None:
+def test_write_run_recovery_file(mock_run: mock.MagicMock) -> None:
     mock_run.id = uuid4().hex
-    mock_run.experiment = mock_experiment
-    mock_experiment.name = uuid4().hex
+    mock_run.experiment.name = uuid4().hex
     expected_run_recovery_id = mock_run.experiment.name + EXPERIMENT_RUN_SEPARATOR + mock_run.id
     himl._write_run_recovery_file(mock_run)
     recovery_file_text = Path(himl.RUN_RECOVERY_FILE).read_text()
     assert expected_run_recovery_id == recovery_file_text
     mock_run.id = uuid4().hex
-    mock_experiment.name = uuid4().hex
+    mock_run.experiment.name = uuid4().hex
     Path(himl.RUN_RECOVERY_FILE).unlink()
     himl._write_run_recovery_file(mock_run)
     recovery_file_text = Path(himl.RUN_RECOVERY_FILE).read_text()
