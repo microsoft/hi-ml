c5a7444f12455558eca087147dfd01a00aa844b9
Tim Regan <timregan@microsoft.com>
2021-08-03 08:42:40 +0000

    more debug lines

--- testhiml/health/azure/test_himl.py
+++ testhiml/health/azure/test_himl.py
@@ -434,6 +434,7 @@ def test_invoking_hello_world_config1(local: bool, tmp_path: Path) -> None:
         assert 'The message was: hello_world' in captured
     else:
         assert "Successfully queued new run test_script_" in captured
+        print(f"tmp_path = {str(tmp_path.absolute())}")
         run = get_most_recent_run(run_recovery_file=tmp_path / himl.RUN_RECOVERY_FILE)
         assert run.status in ["Finalizing", "Completed"]
         log_root = tmp_path / "logs"
