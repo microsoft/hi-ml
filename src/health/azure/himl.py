075419bb2c34ca23c8ebd256b386a919b4225147
Anton Schwaighofer <antonsc@microsoft.com>
2021-08-04 22:38:24 +0100

    fix termination 2

--- src/health/azure/himl.py
+++ src/health/azure/himl.py
@@ -258,7 +258,7 @@ def submit_run(workspace: Workspace,
     print("==============================================================================\n")
     if wait_for_completion:
         print("Waiting for the completion of the AzureML run.")
-        run.wait_for_completion(show_output=wait_for_completion_show_output)
+        run.wait_for_completion(show_output=wait_for_completion_show_output, raise_on_error=True)
         if not is_run_and_child_runs_completed(run):
             raise ValueError(f"Run {run.id} in experiment {run.experiment.name} or one of its child "
                              "runs failed.")
