12bad9ee35a4b8072e1c98975e53f9995889fc07
Tim Regan <timregan@microsoft.com>
2021-08-08 10:21:24 +0100

    Directly swapping to old submit_if_needed

--- src/health/azure/himl.py
+++ src/health/azure/himl.py
@@ -275,7 +275,7 @@ def _str_to_path(s: Optional[PathOrString]) -> Optional[Path]:
     return s
 
 
-def submit_to_azure_if_needed(  # type: ignore
+def submit_to_azure_if_neededNEW(  # type: ignore
         # ignore missing return statement since we 'exit' instead when submitting to AzureML
         compute_cluster_name: str,
         entry_script: Optional[PathOrString] = None,
@@ -439,6 +439,183 @@ def submit_to_azure_if_needed(  # type: ignore
         if after_submission is not None:
             after_submission(run)
         exit(0)
+
+
+def submit_to_azure_if_needed(  # type: ignore # missing return since we exit
+        entry_script: Path,
+        compute_cluster_name: str,
+        conda_environment_file: Path,
+        aml_workspace: Optional[Workspace] = None,
+        workspace_config_path: Optional[Path] = None,
+        snapshot_root_directory: Optional[Path] = None,
+        script_params: Optional[List[str]] = None,
+        environment_variables: Optional[Dict[str, str]] = None,
+        ignored_folders: Optional[List[Path]] = None,
+        default_datastore: str = "",
+        input_datasets: Optional[List[StrOrDatasetConfig]] = None,
+        output_datasets: Optional[List[StrOrDatasetConfig]] = None,
+        num_nodes: int = 1,
+        wait_for_completion: bool = False,
+        wait_for_completion_show_output: bool = False,
+        ) -> AzureRunInformation:
+    """
+    Submit a folder to Azure, if needed and run it.
+
+    Use the flag --azureml to submit to AzureML, and leave it out to run locally.
+
+    :param entry_script: The script that should be run in AzureML
+    :param compute_cluster_name: The name of the AzureML cluster that should run the job. This can be a cluster with
+    CPU or GPU machines.
+    :param conda_environment_file: The conda configuration file that describes which packages are necessary for your
+    script to run.
+
+    :param aml_workspace: There are two optional parameters used to glean an existing AzureML Workspace. The simplest is
+    to pass it in as a parameter.
+    :param workspace_config_file: The 2nd option is to apecify the path to the config.json file downloaded from the
+    Azure portal from which we can retrieve the existing Workspace.
+
+    :param snapshot_root_directory: The directory that contains all code that should be packaged and sent to AzureML.
+    All Python code that the script uses must be copied over.
+    :param ignored_folders: A list of folders to exclude from the snapshot when copying it to AzureML.
+    :param script_params: A list of parameter to pass on to the script as it runs in AzureML. If empty (or None, the
+    default) these will be copied over from sys.argv.
+    :param environment_variables: An optional dictionary of environment varaible that the script relies on.
+
+    :param default_datastore: The data store in your AzureML workspace, that points to your training data in blob
+    storage. This is described in more detail in the README.
+    :param input_datasets: The script will consume all data in folder in blob storage as the input. The folder must
+    exist in blob storage, in the location that you gave when creating the datastore. Once the script has run, it will
+    also register the data in this folder as an AzureML dataset.
+    :param output_datasets: The script will create a temporary folder when running in AzureML, and while the job writes
+    data to that folder, upload it to blob storage, in the data store.
+
+    :param num_nodes: The number of nodes to use in distributed training on AzureML.
+    :param wait_for_completion: If False (the default) return after the run is submitted to AzureML, otherwise wait for
+    the completion of this run (if True).
+    :param wait_for_completion_show_output: If wait_for_completion is True this parameter indicates whether to show the
+    run output on sys.stdout.
+
+    :return: If the script is submitted to AzureML then we terminate python as the script should be executed in AzureML,
+    otherwise we return a AzureRunInformation object.
+    """
+    cleaned_input_datasets = _replace_string_datasets(input_datasets or [],
+                                                      default_datastore_name=default_datastore)
+    cleaned_output_datasets = _replace_string_datasets(output_datasets or [],
+                                                       default_datastore_name=default_datastore)
+
+    if AZUREML_COMMANDLINE_FLAG not in sys.argv[1:]:
+        return AzureRunInformation(
+            input_datasets=[d.local_folder for d in cleaned_input_datasets],
+            output_datasets=[d.local_folder for d in cleaned_output_datasets],
+            run=RUN_CONTEXT,
+            is_running_in_azure=False,
+            output_folder=Path.cwd() / OUTPUT_FOLDER,
+            log_folder=Path.cwd() / LOG_FOLDER
+        )
+
+    in_azure = is_running_in_azure()
+    if in_azure:
+        returned_input_datasets = [RUN_CONTEXT.input_datasets[_input_dataset_key(index)]
+                                   for index in range(len(cleaned_input_datasets))]
+        returned_output_datasets = [RUN_CONTEXT.output_datasets[_output_dataset_key(index)]
+                                    for index in range(len(cleaned_output_datasets))]
+        return AzureRunInformation(
+            input_datasets=returned_input_datasets,
+            output_datasets=returned_output_datasets,
+            run=RUN_CONTEXT,
+            is_running_in_azure=True,
+            output_folder=Path.cwd() / OUTPUT_FOLDER,
+            log_folder=Path.cwd() / LOG_FOLDER
+        )
+
+    if not snapshot_root_directory:
+        raise ValueError("Cannot submit to AzureML without the snapshot_root_directory")
+
+    if workspace_config_path and workspace_config_path.is_file():
+        auth = get_authentication()
+        workspace = Workspace.from_config(path=workspace_config_path, auth=auth)
+    elif aml_workspace:
+        workspace = aml_workspace
+    else:
+        raise ValueError("Cannot glean workspace config from parameters, and so not submitting to AzureML")
+
+    logging.info(f"Loaded: {workspace.name}")
+    environment = Environment.from_conda_specification("simple-env", conda_environment_file)
+
+    # TODO: InnerEye.azure.azure_runner.submit_to_azureml does work here with interupt handlers to kill interupted jobs.
+    # We'll do that later if still required.
+
+    if not script_params:
+        script_params = [p for p in sys.argv[1:] if p != AZUREML_COMMANDLINE_FLAG]
+
+    entry_script_relative = entry_script.relative_to(snapshot_root_directory)
+    run_config = RunConfiguration(
+        script=entry_script_relative,
+        arguments=script_params)
+    run_config.environment = environment
+    print(f"run_config.docker.use_docker == {run_config.docker.use_docker}")
+    print(f"run_config.environment.python.user_managed_dependencies == {run_config.environment.python.user_managed_dependencies}")
+    #run_config.docker.use_docker = True
+
+    if compute_cluster_name not in workspace.compute_targets:
+        raise ValueError(f"Could not find the compute target {compute_cluster_name} in the AzureML workspaces ",
+                         f"{list(workspace.compute_targets.keys())}")
+    script_run_config = ScriptRunConfig(
+        source_directory=str(snapshot_root_directory),
+        run_config=run_config,
+        compute_target=workspace.compute_targets[compute_cluster_name],
+        environment=environment)
+
+    inputs = {}
+    for index, d in enumerate(cleaned_input_datasets):
+        consumption = d.to_input_dataset(workspace=workspace, dataset_index=index)
+        inputs[consumption.name] = consumption
+    outputs = {}
+    for index, d in enumerate(cleaned_output_datasets):
+        out = d.to_output_dataset(workspace=workspace, dataset_index=index)
+        outputs[out.name] = out
+    run_config.data = inputs
+    run_config.output_data = outputs
+
+    experiment_name = to_azure_friendly_string(entry_script.stem)
+    experiment = Experiment(workspace=workspace, name=experiment_name)
+
+    lines_to_append: List[str] = []
+    amlignore_path = snapshot_root_directory or Path.cwd()
+    amlignore_path = amlignore_path / ".amlignore"
+    if ignored_folders:
+        lines_to_append = [str(path) for path in ignored_folders] if ignored_folders else []
+    with append_to_amlignore(
+            amlignore=amlignore_path,
+            lines_to_append=lines_to_append):
+        # TODO: InnerEye.azure.azure_runner.submit_to_azureml does work here with interupt handlers to kill interupted
+        # jobs. We'll do that later if still required.
+
+        run: Run = experiment.submit(script_run_config)
+
+        # These need to be 'print' not 'logging.info' so that the calling script sees them outside AzureML
+        wait_msg = "Waiting for completion of AzureML run" if wait_for_completion else "Not waiting for completion of \
+            AzureML run"
+        print("\n==============================================================================")
+        print(f"Successfully queued new run {run.id} in experiment: {experiment.name}")
+        print("Experiment URL: {}".format(experiment.get_portal_url()))
+        print("Run URL: {}".format(run.get_portal_url()))
+        print(wait_msg)
+        print("==============================================================================\n")
+
+        if wait_for_completion:
+            run.wait_for_completion(show_output=wait_for_completion_show_output)
+
+        if script_params:
+            run.set_tags({"commandline_args": " ".join(script_params)})
+
+        recovery_id = create_run_recovery_id(run)
+        recovery_file = Path(RUN_RECOVERY_FILE)
+        if recovery_file.exists():
+            recovery_file.unlink()
+        recovery_file.write_text(recovery_id)
+
+    exit(0)
 
 
 def _write_run_recovery_file(run: Run) -> None:
