b231861c6f8ec06641fc54966f6766ef8bcb3f64
Jonathan Tripp <v-jontri@microsoft.com>
2021-09-20 14:38:00 +0000

    Move coverage formatting to new action

--- .github/actions/format_coverage/dist/index.js
+++ .github/actions/format_coverage/dist/index.js
@@ -6330,21 +6330,58 @@ module.exports = require("zlib");
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
+//  ------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+//  ------------------------------------------------------------------------------------------
+
 const core = __nccwpck_require__(555);
 const github = __nccwpck_require__(62);
+const { promises: fs } = __nccwpck_require__(747)
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+  async function run() {
+  try {
+    const sha = github.context.eventName == 'pull_request' ? github.context.payload.pull_request.head.sha : github.context.sha
+
+    const coverage = await fs.readFile(core.getInput('file'), 'utf8')
+
+    var table = ""
+    const coverage_lines = coverage.split("\n")
+    for (var i = 0; i < coverage_lines.length; i++)
+    {
+      const cells = coverage_lines[i].split(" ")
+      var row = ""
+      for (var j = 0; j < cells.length; j++)
+      {
+        if (cells[j] != "")
+        {
+          if (row.length != "")
+          {
+            row += "|"
+          }
+          row += cells[j]
+        }
+      }
+      if (row != "")
+      {
+        table += row + "\n"
+      }
+    }
+
+    console.log(`Table: ${table}`);
+
+    await github.repos.createCommitComment({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      commit_sha: sha,
+      body: table
+    })
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
 
 })();
 
--- .github/actions/format_coverage/index.js
+++ .github/actions/format_coverage/index.js
@@ -1,15 +1,52 @@
+//  ------------------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation. All rights reserved.
+//  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+//  ------------------------------------------------------------------------------------------
+
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { promises: fs } = require('fs')
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+  async function run() {
+  try {
+    const sha = github.context.eventName == 'pull_request' ? github.context.payload.pull_request.head.sha : github.context.sha
+
+    const coverage = await fs.readFile(core.getInput('file'), 'utf8')
+
+    var table = ""
+    const coverage_lines = coverage.split("\n")
+    for (var i = 0; i < coverage_lines.length; i++)
+    {
+      const cells = coverage_lines[i].split(" ")
+      var row = ""
+      for (var j = 0; j < cells.length; j++)
+      {
+        if (cells[j] != "")
+        {
+          if (row.length != "")
+          {
+            row += "|"
+          }
+          row += cells[j]
+        }
+      }
+      if (row != "")
+      {
+        table += row + "\n"
+      }
+    }
+
+    console.log(`Table: ${table}`);
+
+    await github.repos.createCommitComment({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      commit_sha: sha,
+      body: table
+    })
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
