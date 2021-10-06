023018ef45f4d1844f5632b46b6747774e117789
Jonathan Tripp <v-jontri@microsoft.com>
2021-10-06 15:08:20 +0000

    Fix merge issues

--- hi-ml-azure/src/health/azure/__init__.py
+++ hi-ml-azure/src/health/azure/__init__.py
@@ -4,8 +4,8 @@
 #  ------------------------------------------------------------------------------------------
 
 from health.azure.azure_util import (fetch_run, set_environment_variables_for_multi_node, split_recovery_id,
-                                     download_from_datastore, upload_to_datastore,
                                      get_most_recent_run, download_files_from_run_id, download_checkpoints_from_run_id,
+                                     download_from_datastore, upload_to_datastore,
                                      run_upload_file, run_upload_folder)
 from health.azure.datasets import DatasetConfig
 from health.azure.himl import (AzureRunInfo, create_run_configuration, create_script_run, get_workspace, submit_run,
