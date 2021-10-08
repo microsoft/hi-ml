861fa8246fcbe5fef59282997f6c39f2ff2b4732
Jonathan Tripp <v-jontri@microsoft.com>
2021-10-08 14:20:19 +0000

    Further compression of upload folder test

--- hi-ml-azure/tests/testazure/test_data/simple/upload_folder.py
+++ hi-ml-azure/tests/testazure/test_data/simple/upload_folder.py
@@ -7,7 +7,6 @@ Testing run_upload_folder.
 """
 
 from pathlib import Path
-import random
 
 from azureml.core.run import Run
 
@@ -26,19 +25,13 @@ def run_test(run: Run) -> None:
     :param run: AzureML run.
     """
     # Create test files.
-    # [0, 12) Create test files in the root of the base_data folder.
-    upload_util.create_test_files(None, range(0, 12))
+    # [0, 9) Create test files in the root of the base_data folder.
+    upload_util.create_test_files(None, range(0, 9))
 
-    # [12, 24) Create test files in a direct sub folder of the base_data folder.
-    upload_util.create_test_files(Path("sub1"), range(12, 24))
-
-    # [24, 36) Create test files in a sub sub sub folder of the base_data folder.
-    upload_util.create_test_files(Path("sub1") / "sub2" / "sub3", range(24, 36))
-
-    # [36, 45) Create test files in a direct sub folder of the base_data folder, with same filenames as the first set.
+    # [9, 18) Create test files in a direct sub folder of the base_data folder, with same filenames as the first set.
     upload_util.create_test_files(Path("sub1"), range(0, 9))
 
-    # [45, 54) Create test files in a sub sub sub folder of the base_data folder, with same filenames as the first set.
+    # [18, 27) Create test files in a sub sub sub folder of the base_data folder, with same filenames as the first set.
     upload_util.create_test_files(Path("sub1") / "sub2" / "sub3", range(0, 9))
 
     # Extract the list of test file names
@@ -47,21 +40,14 @@ def run_test(run: Run) -> None:
     # Split into distinct sets for each stage of the test
     test_file_name_sets = [
         # 0. A set of files at base, sub1, and sub1/sub2/sub3 level to check
-        #  file and folder handling.
-        set(filenames[:3]).union(set(filenames[12:15])).union(set(filenames[24:27])),
+        #  file and folder handling, that all have the same filenames but different folders
+        set(filenames[0:3]).union(set(filenames[9:12])).union(set(filenames[18:21])),
         # 1. A distinct set of files to 0, with different filenames, in the same folders to
         #  check adding distinct files to existing folders.
-        set(filenames[3:6]).union(set(filenames[15:18])).union(set(filenames[27:30])),
-        # 2. A distinct set of files to 0 and 1, but with the same filenames as 0, in different
-        #  folders to check repeating the filename but in a subfolder
-        set(filenames[36:39]).union(set(filenames[45:48])),
-        # 3. A distinct set of files to 0, 1 and 2, with different filenames to check uploading some
+        set(filenames[3:6]).union(set(filenames[12:15])).union(set(filenames[21:24])),
+        # 2. A distinct set of files to 0, 1 and 2, with different filenames to check uploading some
         #  duplicate files and some new files.
-        set(filenames[6:9]).union(set(filenames[18:21])).union(set(filenames[30:33])),
-        # 4. A set of files that all have the same filenames but different folders, that have already been uploaded
-        set(filenames[0:3]).union(set(filenames[36:39])).union(set(filenames[45:48])),
-        # 5. A distinct set of files, all with the same filename, but in different folders.
-        set(filenames[9:12]).union(set(filenames[21:24])).union(set(filenames[33:36]))
+        set(filenames[6:9]).union(set(filenames[15:18])).union(set(filenames[24:27])),
     ]
 
     folder_name = "uploaded_folder"
@@ -72,10 +58,10 @@ def run_test(run: Run) -> None:
 
     # Step 1, upload distinct file sets
     step = 0
-    for i in range(0, 3):
+    for i in range(0, 2):
         # Remove any existing test files
         upload_util.rm_test_file_name_set(test_upload_folder)
-        # Copy in the new test file set
+        # Copy in the new, distinct, test file set
         upload_util.copy_test_file_name_set(test_upload_folder, test_file_name_sets[i])
 
         upload_files = test_file_name_sets[i]
@@ -95,13 +81,15 @@ def run_test(run: Run) -> None:
                                  upload_folder_name=folder_name)
 
     # Step 2, upload the overlapping file sets
-    for (k, i) in [(0, 3), (4, 5)]:
+    for (k, i) in [(0, 2)]:
         upload_util.rm_test_file_name_set(test_upload_folder)
+        # Set k has already been uploaded, these should be silently ignored
         upload_util.copy_test_file_name_set(test_upload_folder, test_file_name_sets[k])
+        # Set i has new files
         upload_util.copy_test_file_name_set(test_upload_folder, test_file_name_sets[i])
 
         upload_files = test_file_name_sets[k].union(test_file_name_sets[i])
-        good_filenames = good_filenames.union(test_file_name_sets[i])
+        good_filenames = good_filenames.union(upload_files)
 
         print(f"Upload file sets {k} and {i}: {upload_files}, \n "
               "this should be fine, since overlaps handled")
@@ -122,14 +110,12 @@ def run_test(run: Run) -> None:
         upload_util.rm_test_file_name_set(test_upload_folder)
         upload_util.copy_test_file_name_set(test_upload_folder, test_file_name_sets[0])
 
-        upload_files = test_file_name_sets[0]
-
-        random_file = random.choice(test_file_name_sets[0])
+        random_file = filenames[k * 9]
         random_upload_file = test_upload_folder / random_file
         existing_text = random_upload_file.read_text()
         random_upload_file.write_text("modified... " + existing_text)
 
-        print(f"Upload file set {k}: {upload_files}, \n"
+        print(f"Upload file set {k}: {random_file}, \n"
               "this should be raise an exception since one of the files has changed")
 
         try:
