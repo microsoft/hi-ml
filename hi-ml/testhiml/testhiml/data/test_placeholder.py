57d44df4a4b64472c2ee5c250db827e50e6fd75c
Anton Schwaighofer <antonsc@microsoft.com>
2021-10-11 15:53:56 +0100

    cleanup

--- hi-ml/testhiml/testhiml/data/test_placeholder.py
+++ hi-ml/testhiml/testhiml/data/test_placeholder.py
@@ -5,18 +5,6 @@
 import pytest
 
 from health_azure.utils import run_duration_string_to_seconds
-from health.data.placeholder import placeholder
-
-
-@pytest.mark.fast
-def test_placeholder_fast() -> None:
-    """A test placeholder test, fast version"""
-    assert placeholder() is True
-
-
-def test_placeholder() -> None:
-    """A test placeholder test"""
-    assert placeholder() is True
 
 
 @pytest.mark.fast
