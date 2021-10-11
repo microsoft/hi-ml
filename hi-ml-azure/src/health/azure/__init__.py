8132ed8a4823947e30dc56ddf1cd47fe2498fd89
Anton Schwaighofer <antonsc@microsoft.com>
2021-10-11 11:38:03 +0100

    adding logger

--- hi-ml-azure/src/health/azure/__init__.py
+++ hi-ml-azure/src/health/azure/__init__.py
@@ -3,26 +3,30 @@
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
 
-from health.azure.azure_util import (fetch_run, set_environment_variables_for_multi_node, split_recovery_id,
-                                     get_most_recent_run, download_files_from_run_id, download_checkpoints_from_run_id,
-                                     download_from_datastore, upload_to_datastore, torch_barrier)
+from health.azure.azure_util import (download_checkpoints_from_run_id, download_files_from_run_id,
+                                     download_from_datastore, fetch_run, get_most_recent_run, is_running_in_azure_ml,
+                                     set_environment_variables_for_multi_node, split_recovery_id, torch_barrier,
+                                     upload_to_datastore)
 from health.azure.datasets import DatasetConfig
 from health.azure.himl import (AzureRunInfo, create_run_configuration, create_script_run, get_workspace, submit_run,
                                submit_to_azure_if_needed)
 
-__all__ = ["fetch_run",
-           "set_environment_variables_for_multi_node",
-           "split_recovery_id",
-           "DatasetConfig",
-           "AzureRunInfo",
-           "create_run_configuration",
-           "create_script_run",
-           "get_workspace",
-           "submit_run",
-           "submit_to_azure_if_needed",
-           "get_most_recent_run",
-           "download_files_from_run_id",
-           "download_checkpoints_from_run_id",
-           "download_from_datastore",
-           "upload_to_datastore",
-           "torch_barrier"]
+__all__ = [
+    "DatasetConfig",
+    "AzureRunInfo",
+    "create_run_configuration",
+    "create_script_run",
+    "download_files_from_run_id",
+    "download_checkpoints_from_run_id",
+    "download_from_datastore",
+    "fetch_run",
+    "get_most_recent_run",
+    "get_workspace",
+    "is_running_in_azure_ml",
+    "set_environment_variables_for_multi_node",
+    "split_recovery_id",
+    "submit_run",
+    "submit_to_azure_if_needed",
+    "torch_barrier",
+    "upload_to_datastore",
+]
--- hi-ml/tests/testhiml/utils/__init__.py
+++ hi-ml/tests/testhiml/utils/__init__.py
@@ -2,11 +2,3 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
-
-from health.utils.logging import AzureMLLogger, log_learning_rate, log_on_epoch
-
-__all__ = [
-    "AzureMLLogger",
-    "log_learning_rate",
-    "log_on_epoch",
-]
