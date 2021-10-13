07c573a1aaaa445562d3cb5b9cb618d68126efce
Melissa Bristow <mebristo@microsoft.com>
2021-10-13 22:32:17 +0100

    Add more report features

---
+++ hi-ml/src/health/utils/reports/reports.py
@@ -0,0 +1,450 @@
+import sys
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+from typing import List, Optional, Tuple, Union, Dict, Any, Iterable, Type, Callable
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+from PIL import Image
+from sklearn.metrics import auc, precision_recall_curve, recall_score, roc_auc_score, roc_curve
+
+from fpdf import FPDF
+
+VALID_PNG_EXTENSIONS = [".png"]
+VALID_NUMPY_EXTENSIONS = (".npy", ".npz")
+
+
+def _file_matches_extension(file: Path, valid_extensions: Iterable[str]) -> bool:
+    """
+    Returns true if the given file name has any of the provided file extensions.
+
+    :param file: The file name to check.
+    :param valid_extensions: A tuple with all the extensions that are considered valid.
+    :return: True if the file has any of the given extensions.
+    """
+    dot = "."
+    extensions_with_dot = tuple(e if e.startswith(dot) else dot + e for e in valid_extensions)
+    return str(file).lower().endswith(extensions_with_dot)
+
+
+def is_numpy_file_path(file: Path) -> bool:
+    """
+    Returns true if the given file name appears to belong to a Numpy file.
+
+    :param file: The file name to check.
+    :return: True if the file name indicates a Numpy file.
+    """
+    return _file_matches_extension(file, VALID_NUMPY_EXTENSIONS)
+
+
+def is_png(file: Path) -> bool:
+    """
+    Returns true if file is png
+    """
+    return _file_matches_extension(file, VALID_PNG_EXTENSIONS)
+
+
+def load_png(path: Path) -> Image:
+    with Image.open(path) as im:
+        yield im
+
+
+def load_numpy_image(path: Path, image_type: Optional[Type] = None) -> Image:
+    image = np.load(path)
+    if type(image) is np.lib.npyio.NpzFile:
+        keys = list(image.keys())
+        assert len(keys) == 1
+        image = image[keys[0]]
+    if image_type is not None:
+        image = image.astype(dtype=image_type)
+    return Image.fromarray(image)
+
+
+def load_image_in_known_formats(file: Path) -> Image:
+    """
+    Loads an image from a file in the given path. At the moment, this supports png and numpy files
+
+    :param file: The path of the file to load.
+    :return: the image
+    """
+    if is_numpy_file_path(file):
+        return load_numpy_image(file)
+    elif is_png(file):
+        return load_png(file)
+    else:
+        raise ValueError(f"Unsupported image file type for path {file}")
+
+
+class Report(FPDF):
+    def __init__(self, title: str = "Default Report", output_folder: str = "outputs"):
+        super().__init__()
+        report_folder = Path(output_folder) / title.lower().replace(" ", "_")
+        report_folder.mkdir(exist_ok=True, parents=True)
+
+        self.title = title
+        self.report_folder = report_folder
+
+    def add_break(self, num_lines: int = 1):
+        # num lines: The number of blank lines to add
+        for _ in range(num_lines):
+            self.ln()
+
+    def add_title_header(self, title_str: str):
+        self.set_table_style(font_style="B", font_size=18)
+        self.cell(txt=title_str, ln=1, align="C", center=True)
+
+    def add_header(self, heading_str: str):
+        self.set_table_style(font_style="B", font_size=16)
+        self.cell(txt=heading_str, ln=1)
+
+    @staticmethod
+    def _reduce_largest_col_with(header_widths: List[float]) -> List[float]:
+        max_header_width = max(header_widths)
+        largest_idx = header_widths.index(max_header_width)
+        header_widths[largest_idx] = max_header_width * 0.8
+        return header_widths
+
+    def get_string_width_(self, x: str) -> int:
+        x = str(x) if not isinstance(x, str) else x
+        string_width = len(x) * self.font_size_pt
+        # add additional leeway for special characters
+        # r = re.compile(r'\[^a-zA-Z !@#$%&*_+-=|:";<>,./\(\)\[]\{\}']')
+        # special_chars = r.findall(x)
+        return np.ceil(string_width)  # + 2 * len(special_chars))
+
+    def _get_column_widths(self, headers: List[str], data: List[List[str]], col_distribution: str = "avg"):
+        # TODO: col distr -> enum
+        page_width = self.epw
+        avg_col_size = page_width / len(headers)
+        if col_distribution == "avg":
+            widths = [avg_col_size] * len(headers)
+        elif col_distribution == "fitted":
+            # num_chars = len(header_text)
+            header_widths = [self.get_string_width_(header_text) for header_text in headers]
+            data_widths = [self.get_string_width_(header_text) for header_text in data[0]]
+            widths = [max(header_widths[i], data_widths[i]) for i in range(len(header_widths))]
+            while sum(widths) > page_width:
+                widths = self._reduce_largest_col_with(widths)
+        else:
+            raise ValueError("col_distribution must be one of avg or fitted")
+        return widths
+
+    def set_table_style(self,
+                        fill_color: Tuple[int, int, int] = (255, 255, 255),
+                        text_color: int = 0,
+                        line_color: Tuple[int, int, int] = (0, 0, 0),
+                        line_width: float = 0.3,
+                        font_style: str = "",
+                        font_family: str = "helvetica",
+                        font_size: int = 10) -> None:
+        # set the table style, including background, font and line colours, line width and font style
+        self.set_fill_color(*fill_color)
+        self.set_text_color(text_color)
+        self.set_draw_color(*line_color)
+        self.set_line_width(line_width)  # type: ignore
+        self.set_font(style=font_style, family=font_family, size=font_size)
+
+    def _populate_table_rows(self, headers: List[str], data: List[List[str]], col_widths: List[float],
+                             table_line_height: int, alternate_fill=True, border="LR"):
+        # Data
+        fill = False
+        for row in data:
+            for i in range(len(headers)):
+                row_data = row[i]
+                row_text = str(row_data) if not isinstance(row_data, str) else row_data
+                self.cell(col_widths[i], table_line_height, txt=row_text, border=border, ln=0, align="L", fill=fill)
+            self.ln()
+            # change fill for next line
+            fill = not fill if alternate_fill else False
+
+        # self.cell(sum(w), 0, "", "T", ln=1)
+        self.add_break(num_lines=1)
+
+    def add_table(self, headers: List[str], data: List[List[str]], header_color=(0, 0, 255), header_font_color=0,
+                  header_line_color=(0, 0, 0), header_line_width=0.3, body_line_color=(0, 0, 0), body_line_width=0.3,
+                  alternate_fill=True, alternate_fill_color=(224, 235, 255)):
+
+        self.set_table_style(fill_color=header_color, text_color=header_font_color, font_style="B",
+                             line_color=header_line_color, line_width=header_line_width)
+
+        # Colors, line width and bold font
+        table_line_height = 7
+
+        col_widths = self._get_column_widths(headers, data, col_distribution="fitted")
+
+        for width, header_text in zip(col_widths, headers):
+            # self.cell(w=width, h=7, txt=header_text, border=1, align="C", fill=True)
+            self.cell(w=width, h=table_line_height, txt=header_text, fill=True)
+
+        self.ln()
+        self.set_table_style(fill_color=alternate_fill_color, line_color=body_line_color, line_width=body_line_width)
+
+        self._populate_table_rows(headers, data, col_widths, table_line_height, alternate_fill=alternate_fill,
+                                  border=None)
+
+    def add_table_from_dataframe(self, df: pd.DataFrame, cols_to_print: Optional[List[str]] = None,
+                                 print_index: bool = False, override_headers: List[str] = None):
+
+        if cols_to_print:
+            df = df[cols_to_print]
+            headers = cols_to_print
+        else:
+            headers = df.columns.tolist()
+
+        if print_index:
+            # df = df.reset_index()
+            index_col_name = "index"
+            df.insert(0, index_col_name, np.arange(1, len(df) + 1))
+            # Add the new index column to the header list
+            headers = [index_col_name] + headers
+
+        if override_headers:
+            headers = override_headers
+
+        # read headers from table
+        data = df.to_numpy().tolist()
+        assert len(headers) == len(data[0])
+        self.add_table(headers, data)
+
+    def read_table_from_file(self, data_path: Path) -> pd.DataFrame:
+        suffix = data_path.suffix
+        if suffix == ".csv":
+            df = pd.read_csv(data_path)
+        elif suffix in [".xls", ".xlsx"]:
+            df = pd.read_excel(data_path)
+        else:
+            # TODO: load more file types
+            # with open(data_path, "r") as f_path:
+            #     txt = f_path.read()
+            #     lines = txt.split()
+            raise ValueError(f"Can only read data from .csv, .xls or .xlsx files. Found {suffix}")
+        return df
+
+    def add_table_from_file(self, data_path: Path):
+        df = self.read_table_from_file(data_path)
+        self.add_table_from_dataframe(df)
+
+    def save_report(self) -> Path:
+        report_path = (self.report_folder / self.title.replace(" ", "_").lower()).with_suffix(".pdf")
+        self.output(str(report_path))
+        return report_path
+
+    def add_line_chart_from_file(self, data_path: Path, x_col: str, y_col: str, chart_title: str = "",
+                                 image_height: int = 0, image_width: int = 0, alt_text: str = "") -> None:
+        df = self.read_table_from_file(data_path)
+        x = df[x_col].values
+        y = df[y_col].values
+        self.add_line_chart(x, y, chart_title=chart_title, x_label=x_col, y_label=y_col, image_height=image_height,
+                            image_width=image_width, alt_text=alt_text)
+
+    def add_image(self, image_path):
+        # image: Image = load_image_in_known_formats(image_path)
+        self.image(image_path)
+
+
+class Plot:
+    def __init__(self, fig_folder, fig_title=None, n_rows=1, n_cols=1, figsize=(6.4, 4.8)):
+        self.fig_folder = fig_folder
+        self.fig_title = fig_title or "plot"
+        fig, axs = plt.subplots(n_rows, n_cols, figsize=figsize)
+        self.fig = fig
+        # axes must be a list of lists of Axes objects
+        if isinstance(axs, Axes):
+            axes = [[axs]]
+        elif isinstance(axs, np.ndarray):
+            axes = axs.tolist()
+            if isinstance(axes[0], Axes):
+                axes = [axes]
+        self.axs = axes
+        assert isinstance(self.axs, List)
+        assert isinstance(self.axs[0], List)
+        assert isinstance(self.axs[0][0], Axes)
+        self.current_axis = None
+
+    @property
+    def next_empty_axis(self):
+        for ax_list in self.axs:
+            for ax in ax_list:
+                if not ax.has_data():
+                    self.current_axis = ax
+                    return ax
+        raise ValueError("Trying to add more plots than originally specified")
+
+    @staticmethod
+    def label_plot(ax, chart_title: str = "", x_label: str = "", y_label: str = ""):
+        if chart_title:
+            ax.set_title(chart_title)
+        if x_label:
+            ax.set_xlabel(x_label)
+        if y_label:
+            ax.set_ylabel(y_label)
+        return ax
+
+    def save_chart(self) -> Path:
+        plt.tight_layout()
+        plot_path = (self.fig_folder / self.fig_title.lower().replace(" ", "_")).with_suffix(".png")
+        self.fig.savefig(plot_path)
+        return plot_path
+
+    def add_line(self, x, y, chart_title, x_label, y_label, superimpose=False):
+        if self.current_axis is None or superimpose is False:
+            ax = self.next_empty_axis
+        else:
+            ax = self.current_axis
+
+        ax.plot(x, y)
+        self.label_plot(ax, chart_title=chart_title, x_label=x_label, y_label=y_label)
+
+    def add_pr_curve(self, model_outputs, labels, plot_kwargs=None, superimpose=False):
+        if self.current_axis is None or superimpose is False:
+            ax = self.next_empty_axis
+        else:
+            ax = self.current_axis
+
+        plot_kwargs = {} if plot_kwargs is None else {}
+
+        fpr, tpr, thresholds = roc_curve(labels, model_outputs)
+        ax.plot(fpr, tpr, **plot_kwargs)
+        ax.set_xlabel("Recall")
+        ax.set_ylabel("Precision")
+        ax.set_title("PR Curve")
+        return ax
+
+    def add_roc_curve(self, model_outputs, labels, plot_kwargs=None, superimpose=False):
+        if self.current_axis is None or superimpose is False:
+            ax = self.next_empty_axis
+        else:
+            ax = self.current_axis
+
+        plot_kwargs = {} if plot_kwargs is None else {}
+
+        precision, recall, thresholds = precision_recall_curve(labels, model_outputs)
+        ax.plot(recall, precision, **plot_kwargs)
+        ax.set_xlabel("False positive rate")
+        ax.set_ylabel("True positive rate")
+        ax.set_title("ROC curve")
+        return ax
+
+    def plot_scores_and_summary(self, labels, model_outputs,
+                                scoring_fn: Callable[[Tuple[List[Any], List[Any]]], Tuple[np.ndarray, np.ndarray]],
+                                interval_width: float = .8,
+                                ax: Optional[Axes] = None) -> Tuple[List, Any]:
+        """
+
+        """
+        if ax is None:
+            ax = plt.gca()
+        x_grid = np.linspace(0, 1, 101)
+        interp_ys = []
+        line_handles = []
+        for index, (lbls, mdl_out) in enumerate(zip(labels, model_outputs)):
+            x_values, y_values = scoring_fn(lbls, mdl_out)
+            interp_ys.append(np.interp(x_grid, x_values, y_values))
+            handle, = ax.plot(x_values, y_values, lw=1)
+            line_handles.append(handle)
+
+        interval_quantiles = [.5 - interval_width / 2, .5, .5 + interval_width / 2]
+        y_lo, y_mid, y_hi = np.quantile(interp_ys, interval_quantiles, axis=0)
+        h1 = ax.fill_between(x_grid, y_lo, y_hi, color='k', alpha=.2, lw=0)
+        h2, = ax.plot(x_grid, y_mid, 'k', lw=2)
+        summary_handle = (h1, h2)
+        return line_handles, summary_handle
+
+    def add_pr_roc_curve_crossval(self, labels, model_outputs, superimpose=True) -> None:
+
+        def get_roc_xy(labels, model_outputs) -> Tuple[np.ndarray, np.ndarray]:
+            fpr, tpr, thresholds = roc_curve(labels, model_outputs)
+            return fpr, tpr
+
+        def get_pr_xy(labels, model_outputs) -> Tuple[np.ndarray, np.ndarray]:
+            precision, recall, thresholds = precision_recall_curve(labels, model_outputs)
+            return recall[::-1], precision[::-1]  # inverted to be in ascending order
+
+        if self.current_axis is None or superimpose is False:
+            ax = self.next_empty_axis
+        else:
+            ax = self.current_axis
+
+        interval_width = .8
+        line_handles, summary_handle = self.plot_scores_and_summary(labels, model_outputs,
+                                                                    scoring_fn=get_roc_xy, ax=ax,
+                                                                    interval_width=interval_width)
+        line_labels = [f"Split {split_index}" for split_index in range(len(labels))]
+        ax.legend(line_handles + [summary_handle],
+                  line_labels + [f"Median \u00b1 {50 * interval_width:g}%"])
+
+        # plot PR curve
+        self.plot_scores_and_summary(labels, model_outputs, scoring_fn=get_pr_xy, ax=self.next_empty_axis,
+                                     interval_width=interval_width)
+
+        # format_pr_or_roc_axes('roc', axs[0])
+        # format_pr_or_roc_axes('pr', axs[1])
+
+        plt.show()
+
+    def add_box_plot(self, data, labels, title="", x_label="", y_label="", superimpose=False):
+        if self.current_axis is None or superimpose is False:
+            ax = self.next_empty_axis
+        else:
+            ax = self.current_axis
+
+        ax.boxplot(data, labels, patch_artist=True, showmeans=True, meanline=True)
+        ax.set_title(title)
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+        # ax.set_xticks()
+        ax.grid()
+
+    def custom_plot(self, plot_name, plot_args=(), plot_kwargs={}, title="", x_label="", y_label="", superimpose=False):
+        if self.current_axis is None or superimpose is False:
+            ax = self.next_empty_axis
+        else:
+            ax = self.current_axis
+
+        if not hasattr(ax, plot_name):
+            raise ValueError(f"Unrecognised attribute {plot_name} for class {type(ax)}")
+
+        callable = getattr(ax, plot_name)
+        callable(*plot_args, **plot_kwargs)
+
+        ax.set_title(title)
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+
+
+def get_data_from_tensorboard_log():
+    # TOOD
+    pass
+
+
+def initialize_report(report_title: str = "Default Report", output_folder: str = "reports") -> Report:
+    report = Report(title=report_title, output_folder=output_folder)
+    report.add_page()
+    # Set the header style
+    report.set_table_style(font_style="B", font_size=16)
+    report.add_title_header(report_title)
+    report.add_break(num_lines=1)
+    # Reset to default page style
+    report.set_table_style()
+    report.set_title(report_title)
+    return report
+
+
+def parse_arguments(args: List[str]) -> Namespace:
+    # Parse command line arguments for generating reports
+    parser = ArgumentParser()
+    parser.add_argument("--output_folder", type=str, default="reports",
+                        help="The folder in which to store generated reports")
+    parser.add_argument("--report_title", type=str, default=None, help="The title of the report")
+    args = parser.parse_args()
+    return args
+
+#
+# def main():
+#     args = parse_arguments(sys.argv[1:])
+#
+#
+# if __name__ == "__main__":
+#     main()
---
+++ hi-ml/tests/testhiml/test_utils/test_reports.py
@@ -0,0 +1,234 @@
+import sys
+from argparse import Namespace
+
+from pathlib import Path
+from unittest.mock import MagicMock, patch
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from PIL import Image
+
+
+from health.utils.reports import reports as report_util
+
+
+@pytest.mark.parametrize("args, attr_name, attr_val", [
+    (["", "--output_folder", "report_folder"], "output_folder", "report_folder"),
+    ([""], "output_folder", "reports"),
+    # ([], "", "")
+])
+def test_create_args(args, attr_name, attr_val):
+    with patch.object(sys, 'argv', args):
+        args = report_util.parse_arguments(sys.argv[1:])
+        assert hasattr(args, attr_name)
+        assert getattr(args, attr_name) == attr_val
+
+
+# def test_add_table_from_dataframe(tmp_path: Path):
+#     df = pd.DataFrame({
+#         "X": [1, 2, 3],
+#         "Y": ["a", "b", "c"]
+#     })
+#     report_title = "My Report"
+#     report_output_folder = tmp_path / "reports"
+#     report = report_util.initialize_report(report_title=report_title, output_folder= report_output_folder)
+#
+#     report.add_table_from_dataframe(df)
+#
+#     report_path = report.save_report()
+#     stripped_title = report_title.lower().replace(" ", "_")
+#     expected_report_path = report_output_folder / stripped_title / (stripped_title + ".pdf")
+#
+#     assert report_path == expected_report_path
+#     assert expected_report_path.exists()
+#     assert expected_report_path.stat().st_size > 0
+
+
+@pytest.mark.parametrize("string_to_test, font_size, expected_width", [
+    # ("abc", 10, 30),
+    ("16/02/2015", 5, 50)
+])
+def test_get_string_width_(string_to_test: str, font_size: int, expected_width: int):
+    report = report_util.Report()
+    report.set_font_size(font_size)
+    assert report.get_string_width_(string_to_test) == expected_width
+
+# -------------------
+# GENERATE DUMMY REPORT
+# -------------------
+
+def dummy_table(report):
+    headers = ["Experiment", "Epochs", "loss", "Average ROC AUC"]
+    data = [[1, 150, 5, 0.8], [2, 150, 4.9, 0.91]]
+    df = pd.DataFrame(data=data, columns=headers)
+    report.add_table_from_dataframe(df)
+
+
+def dummy_performers_table(report):
+    headers = ["Top 5 false positives"]
+    data = [['Id 11 score 1.0'],['Id 2 score 0.9'], ['Id 3 score 0.89'], ['Id 4 score 0.85']]
+    report.add_table(headers, data, header_color=(255, 255, 255), alternate_fill=False, body_line_width=0.0)
+
+
+def dummy_line_subplot(report):
+    x = [1, 2, 3]
+    y = [0.7, 0.77, 0.8]
+    chart_title = "Line chart"
+    x_label = "X"
+    y_label = "Y"
+    alt_text = ""
+    image_width = 200
+    image_height = 150
+
+    plot = report_util.Plot(n_cols=2, n_rows=2, figsize=(12.8, 9.0), fig_folder=report.report_folder,
+                            fig_title=chart_title)
+
+    # hack since plots always seem stretched:
+    # image_height = 0.8 * image_width if image_height == image_width else image_height
+
+    plot.add_line(x, y, "plot 1", x_label, y_label)
+    plot.add_line(x, y, "plot 2", x_label, y_label)
+    plot.add_line(x, y, "plot 3", x_label, y_label)
+    plot.add_line(x, y, "plot 4", x_label, y_label)
+
+    plot_path = plot.save_chart()
+
+    report.image(str(plot_path), x=None, y=None, h=image_height, w=image_width, alt_text=alt_text)
+
+
+def dummy_pr_roc_curves(report):
+    model_outputs = np.random.randint(0, 2, size=200)
+    labels = np.random.randint(0, 2, size=200)
+    chart_title = "PR ROC"
+    image_width = 200
+    image_height = 100
+    alt_text = "ROC PR plots"
+
+    plot = report_util.Plot(n_cols=2, figsize=(12.8, 6.0), fig_folder=report.report_folder, fig_title=chart_title)
+
+    plot.add_roc_curve(model_outputs, labels)
+    plot.add_pr_curve(model_outputs, labels)
+
+    plot_path = plot.save_chart()
+
+    report.image(plot_path, x=None, y=None, h=image_height, w=image_width, alt_text=alt_text)
+
+
+def dummy_pr_roc_curves_crossval(report):
+    model_outputs = np.random.randint(0, 2, size=[5, 200])
+    labels = np.random.randint(0, 2, size=[5, 200])
+    chart_title = "PR ROC Crossval"
+    image_width = 200
+    image_height = 100
+    alt_text = "ROC PR plots Cross val"
+
+    plot = report_util.Plot(n_cols=2, figsize=(12.8, 6.0), fig_folder=report.report_folder, fig_title=chart_title)
+
+    plot.add_pr_roc_curve_crossval(model_outputs, labels)
+
+    plot_path = plot.save_chart()
+
+    report.image(plot_path, x=None, y=None, h=image_height, w=image_width, alt_text=alt_text)
+
+def dummy_boxplot(report):
+    num_boxes = 5
+    spreads = [np.random.rand(50) * 100 for _ in range(num_boxes)]
+    centers = [np.ones(25) * 50 for _ in range(num_boxes)]
+    flier_highs = [np.random.rand(10) * 100 + 100 for _ in range(num_boxes)]
+    flier_lows = [np.random.rand(10) * -100 for _ in range(num_boxes)]
+    data = [np.concatenate((spread, center, flier_high, flier_low)) for spread, center, flier_high, flier_low
+            in zip(spreads, centers, flier_highs, flier_lows)]
+
+    labels = np.arange(num_boxes).tolist()
+    chart_title = "Dice"
+    image_width = 200
+    image_height = 100
+    alt_text = "dice plot"
+
+    plot = report_util.Plot(figsize=(12.8, 6.0), fig_folder=report.report_folder, fig_title=chart_title)
+    plot.add_box_plot(data, labels, title=chart_title, y_label="Dice")
+
+    plot_path = plot.save_chart()
+    report.image(plot_path, x=None, y=None, h=image_height, w=image_width, alt_text=alt_text)
+
+
+def dummy_custom_plot(report):
+    chart_title = "Scatter plot"
+    image_width = 200
+    image_height = 100
+    alt_text = "scatter plot"
+    plot = report_util.Plot(figsize=(12.8, 6.0), fig_folder=report.report_folder, fig_title=chart_title)
+
+    for color in ['tab:blue', 'tab:orange', 'tab:green']:
+        x = np.random.randint(0, 20, 20).tolist()
+        y = np.random.randint(0, 20, 20).tolist()
+        scale = 200.0 * np.random.rand(20)
+        plot.custom_plot("scatter", (x, y), {'c': color, 's': scale, 'label':color},
+                         x_label="X", y_label="Y", title="My chart", superimpose=True)
+
+    plot_path = plot.save_chart()
+    report.image(plot_path, x=None, y=None, h=image_height, w=image_width, alt_text=alt_text)
+
+
+def dummy_image(output_folder, report):
+    img_paths = []
+    for i in range(1):
+        img_path = Path(output_folder) / f"img_{i}.png"
+        img_array = np.random.randint(0, 255, [250, 250]).astype(np.uint8)
+        np.save(str(str(img_path).split(".")[0]), img_array)
+        im = Image.fromarray(img_array)
+        im.save(img_path, "PNG")
+        img_paths.append(str(img_path))
+
+    report.add_image(img_paths[0])
+
+
+def generate_dummy_report(args: Namespace) -> None:
+    # Generate PDF report
+    report = report_util.initialize_report(report_title = args.report_title, output_folder = args.output_folder)
+
+    report.cell(txt="A short description of this report", center=True, align="C")
+    report.add_break(2)
+
+    dummy_table(report)
+
+    dummy_line_subplot(report)
+    report.add_break()
+
+    dummy_pr_roc_curves(report)
+    report.add_break()
+
+    dummy_boxplot(report)
+    report.add_break()
+
+    dummy_custom_plot(report)
+    report.add_break()
+
+    dummy_image(args.output_folder, report)
+    report.add_break
+
+    dummy_pr_roc_curves_crossval(report)
+    report.add_break()
+
+    dummy_performers_table(report)
+
+    report.cell(txt="End of report", ln=1)
+    report.save_report()
+
+
+def test_generate_dummy_report(tmp_path: Path):
+    report_title = "Dummy report"
+
+    args = ["", "--output_folder", str(tmp_path), "--report_title", report_title]
+    with patch.object(sys, 'argv', args):
+        args = report_util.parse_arguments(sys.argv[1:])
+    generate_dummy_report(args)
+
+    report_title_formatted = report_title.lower().replace(" ", "_")
+
+    expected_files = [report_title_formatted + '.pdf', 'dice.png', 'line_chart.png', 'pr_roc.png', 'scatter_plot.png']
+    for expected_file in expected_files:
+        expected_file_path = tmp_path / report_title_formatted / expected_file
+        assert expected_file_path.exists()
