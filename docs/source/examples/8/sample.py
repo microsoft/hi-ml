e2a7b01b5700a1b764ce857a09fc49fd8f07df39
Jonathan Tripp <v-jontri@microsoft.com>
2021-10-27 18:04:37 +0000

    Minor fixups

--- docs/source/examples/8/sample.py
+++ docs/source/examples/8/sample.py
@@ -52,8 +52,8 @@ def main() -> None:
                         help='Penalty parameter of the error term')
 
     args = parser.parse_args()
-    run.log(f'Kernel type{args.kernel}')
-    run.log(f'Penalty {args.penalty}')
+    run.log('Kernel type', np.str(args.kernel))
+    run.log('Penalty', np.float(args.penalty))
 
     # X -> features, y -> label
     input_folder = run_info.input_datasets[0] or Path("inputs")
@@ -71,7 +71,7 @@ def main() -> None:
     # model accuracy for X_test
     accuracy = svm_model_linear.score(X_test, y_test)
     print('Accuracy of SVM classifier on test set: {:.2f}'.format(accuracy))
-    run.log(f'Accuracy {accuracy}')
+    run.log('Accuracy', np.float(accuracy))
     # creating a confusion matrix
     cm = confusion_matrix(y_test, svm_predictions)
     print(cm)
