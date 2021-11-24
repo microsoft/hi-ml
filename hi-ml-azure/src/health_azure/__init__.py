bb1c9170aac3fe46bf31a3e72fb148c141db3e84
Anton Schwaighofer <antonsc@microsoft.com>
2021-11-24 12:22:00 +0000

    import fix

--- hi-ml-azure/src/health_azure/__init__.py
+++ hi-ml-azure/src/health_azure/__init__.py
@@ -3,18 +3,20 @@
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
 
-from health_azure.utils import (download_checkpoints_from_run_id, download_files_from_run_id,
-                                download_from_datastore, fetch_run, get_most_recent_run, is_running_in_azure_ml,
-                                set_environment_variables_for_multi_node, split_recovery_id, torch_barrier,
-                                upload_to_datastore, RUN_CONTEXT, aggregate_hyperdrive_metrics)
 from health_azure.datasets import DatasetConfig
-from health_azure.himl import (AzureRunInfo, create_run_configuration, create_script_run, get_workspace, submit_run,
-                               submit_to_azure_if_needed, create_crossval_hyperdrive_config)
+from health_azure.himl import (AzureRunInfo, create_crossval_hyperdrive_config, create_run_configuration,
+                               create_script_run, get_workspace, submit_run, submit_to_azure_if_needed)
+from health_azure.utils import (RUN_CONTEXT, aggregate_hyperdrive_metrics, create_aml_run_object,
+                                download_checkpoints_from_run_id, download_files_from_run_id, download_from_datastore,
+                                fetch_run, get_most_recent_run, is_running_in_azure_ml,
+                                set_environment_variables_for_multi_node, split_recovery_id, torch_barrier,
+                                upload_to_datastore)
 
 __all__ = [
     "AzureRunInfo",
     "DatasetConfig",
     "RUN_CONTEXT",
+    "create_aml_run_object",
     "create_run_configuration",
     "create_script_run",
     "download_files_from_run_id",
