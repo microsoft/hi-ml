743b9bbb88b63d97be23fbdaa14b9cb44343f631
Anton Schwaighofer <antonsc@microsoft.com>
2022-01-25 10:23:30 +0000

    set_model_to_eval

--- hi-ml/src/health_ml/utils/common_utils.py
+++ hi-ml/src/health_ml/utils/common_utils.py
@@ -1,4 +1,10 @@
-from typing import Optional
+#  ------------------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+#  ------------------------------------------------------------------------------------------
+
+from contextlib import contextmanager
+from typing import Generator, Optional
 
 import torch
 
@@ -9,3 +15,16 @@ def _create_generator(seed: Optional[int] = None) -> torch.Generator:
         seed = int(torch.empty((), dtype=torch.int64).random_().item())
     generator.manual_seed(seed)
     return generator
+
+
+@contextmanager
+def set_model_to_eval_mode(model: torch.nn.Module) -> Generator:
+    """
+    Puts the given torch model into eval mode. At the end of the context, resets the state of the training flag to
+    what is was before the call.
+    :param model: The model to modify.
+    """
+    old_mode = model.training
+    model.eval()
+    yield
+    model.train(old_mode)
