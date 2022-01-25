d6989facd3bca10484627785dfad1e74eaf95919
Anton Schwaighofer <antonsc@microsoft.com>
2022-01-25 10:24:47 +0000

    basics of object traversal

---
+++ hi-ml-azure/src/health_azure/traverse.py
@@ -0,0 +1,34 @@
+#  ------------------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+#  ------------------------------------------------------------------------------------------
+from typing import Any, Dict, Union
+from ruamel import yaml
+
+
+def is_basic_field(o: Any) -> bool:
+    return isinstance(o, (str, int, float))
+
+
+def _object_to_dict(o: Any) -> Union[str, Dict[str, Any]]:
+    if is_basic_field(o):
+        return o
+    try:
+        fields = vars(o)
+        return {field: _object_to_dict(value) for field, value in fields.items()}
+    except TypeError:
+        return repr(o)
+
+
+def object_to_dict(o: Any) -> Dict[str, Any]:
+    if is_basic_field(o):
+        raise ValueError("This function cannot be used on objects that are basic datatypes.")
+    try:
+        fields = vars(o)
+    except TypeError:
+        raise ValueError("This function cannot be used on objects that do not support the 'vars' operation")
+    return {field: _object_to_dict(value) for field, value in fields.items()}
+
+
+def object_to_yaml(o: Any) -> str:
+    return yaml.safe_dump(object_to_dict(o), default_flow_style=False)
---
+++ hi-ml-azure/testazure/testazure/test_traverse.py
@@ -0,0 +1,47 @@
+#  ------------------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+#  ------------------------------------------------------------------------------------------
+from dataclasses import dataclass
+
+from health_azure.traverse import object_to_dict, object_to_yaml
+
+
+@dataclass
+class OptimizerConfig:
+    learning_rate: float = 1e-3
+    optimizer: str = "Adam"
+
+
+@dataclass
+class TransformConfig:
+    blur_sigma: float = 0.1
+    blur_p: float = 0.2
+    nested: OptimizerConfig = OptimizerConfig()
+
+
+@dataclass
+class FullConfig:
+    optimizer: OptimizerConfig = OptimizerConfig()
+    transforms: TransformConfig = TransformConfig()
+
+
+def test_traverse1() -> None:
+    config = TransformConfig()
+    d = object_to_dict(config)
+    assert d == {"blur_sigma": 0.1, "blur_p": 0.2}
+
+
+def test_traverse2() -> None:
+    config = FullConfig()
+    d = object_to_dict(config)
+    assert d == {
+        "optimizer": {"learning_rate": 1e-3, "optimizer": "Adam"},
+        "transforms": {"blur_sigma": 0.1, "blur_p": 0.2}
+    }
+
+
+def test_to_yaml() -> None:
+    config = FullConfig()
+    yaml = object_to_yaml(config)
+    print(yaml)
