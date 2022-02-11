083571f62fe5f933780f943498fef089da2a608c
Melissa Bristow <mebristo@microsoft.com>
2022-02-11 16:24:38 +0000

    Debug build test failing

--- hi-ml-histopathology/testSSL/conftest.py
+++ hi-ml-histopathology/testSSL/conftest.py
@@ -13,10 +13,12 @@ from typing import Generator
 
 import pytest
 
-from testSSL.testSSL.utils import tests_root_directory
-tests_root = tests_root_directory()
 # temporary workaround until these hi-ml package release
-himl_root = tests_root.parent.parent
+testSSL_root_dir = Path(__file__).parent
+print(f"Adding {testSSL_root_dir} to sys path")
+sys.path.insert(0, str(testSSL_root_dir))
+
+himl_root = testSSL_root_dir.parent.parent
 himl_package_root = himl_root / "hi-ml" / "src"
 logging.info(f"Adding {str(himl_package_root)} to path")
 sys.path.insert(0, str(himl_package_root))
@@ -24,7 +26,7 @@ himl_azure_package_root = himl_root / "hi-ml-azure" / "src"
 logging.info(f"Adding {str(himl_azure_package_root)} to path")
 sys.path.insert(0, str(himl_azure_package_root))
 from health_ml.utils.fixed_paths import OutputFolderForTests  # noqa: E402
-from testSSL.testSSL.test_ssl_containers import create_cxr_test_dataset  # noqa: E402
+from testSSL.test_ssl_containers import create_cxr_test_dataset  # noqa: E402
 
 RELATIVE_TEST_OUTPUTS_PATH = "test_outputs"
 TEST_OUTPUTS_PATH = tests_root_directory() / RELATIVE_TEST_OUTPUTS_PATH
