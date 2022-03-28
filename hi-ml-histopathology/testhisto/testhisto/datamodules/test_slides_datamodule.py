b6c8a0e1554bda0015b7183abcbbdb5b358a3b98
Kenza <t-kbouzid@microsoft.com>
2022-03-28 17:00:26 +0000

    fix linting

--- hi-ml-histopathology/testhisto/testhisto/datamodules/test_slides_datamodule.py
+++ hi-ml-histopathology/testhisto/testhisto/datamodules/test_slides_datamodule.py
@@ -2,14 +2,13 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
-
+import os
 import pandas as pd
+import numpy as np
 
 from pathlib import Path
 from typing import Any, Tuple, Union, Optional
 
-import pytest
-from torch.utils.data import dataloader
 from histopathology.datamodules.base_module import SlidesDataModule
 from histopathology.datasets.base_dataset import SlidesDataset
 
@@ -32,26 +31,29 @@ class MockSlidesDataset(SlidesDataset):
 
 
 class MockSlidesDataModule(SlidesDataModule):
-    def __init__(**kwargs: Any) -> None:
+
+    def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
 
-    def _get_slides_dataset_class(self) -> None:
+    def _get_slides_dataset_class(self) -> MockSlidesDataset:
         return MockSlidesDataset
 
     def get_splits(self) -> Tuple[MockSlidesDataset, MockSlidesDataset, MockSlidesDataset]:
         return (MockSlidesDataset(self.root_path), MockSlidesDataset(self.root_path), MockSlidesDataset(self.root_path))
 
 
-@pytest.mark.parametrize("level", [(0,), (1,), (2,)])
+# @pytest.mark.parametrize("level", [(0,), (1,), (2,)])
 def test_tiling_on_the_fly(level: int = 0) -> None:
-    datamodule = MockSlidesDataModule(root_path=MOCK_DATA_PATH, batch_size=1, tile_count=16, tile_size=28, level=level)
+    datamodule = MockSlidesDataModule(root_path=MOCK_DATA_PATH, batch_size=1, tile_count=16, tile_size=28, level=0)
     dataloader = datamodule._get_dataloader(stage="train", shuffle=False)
     sample = next(iter(dataloader))
     tiles = sample["image"]
-    wsi_id = sample["slide_id"].split("/")[0]
-    patches = np.array([])
+    wsi_id = sample["slide_id"]
+    patches = np.array([np.load(os.path.join(wsi_id, f"patch_{i}.npy")) for i in range(4)])
     assert tiles.shape == (1, 16, 3, 28, 28)
+    for i in range(0, 16, 4):
+        assert (patches[i // 4] == tiles[0, i]).all()
 
 
-def test_overlapping_tiling()->None:
+def test_overlapping_tiling() -> None:
     pass
