0f209d7063afd2a1ce715b40ca1b7bb6d7c97676
Kenza <t-kbouzid@microsoft.com>
2022-03-29 14:15:13 +0000

    save top and bottom tiles thumbnail working after merge from master

--- hi-ml-histopathology/src/histopathology/utils/output_utils.py
+++ hi-ml-histopathology/src/histopathology/utils/output_utils.py
@@ -374,11 +374,11 @@ class DeepMILOutputsHandler:
         save_outputs_and_features(results, outputs_dir)
 
         print("Selecting tiles ...")
-        # selected_slide_ids = save_top_and_bottom_tiles(results, n_classes=self.n_classes, figures_dir=figures_dir)
+        selected_slide_ids = save_top_and_bottom_tiles(results, n_classes=self.n_classes, figures_dir=figures_dir)
 
-        # if self.slides_dataset is not None:
-        #     save_slide_thumbnails_and_heatmaps(results, selected_slide_ids, tile_size=self.tile_size, level=self.level,
-        #                                        slides_dataset=self.slides_dataset, figures_dir=figures_dir)
+        if self.slides_dataset is not None:
+            save_slide_thumbnails_and_heatmaps(results, selected_slide_ids, tile_size=self.tile_size, level=self.level,
+                                               slides_dataset=self.slides_dataset, figures_dir=figures_dir)
 
         save_scores_histogram(results, figures_dir=figures_dir)
 
