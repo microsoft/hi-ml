5f7566a70d983f86d1d444c5e909e79a9c1fd8da
Kenza <t-kbouzid@microsoft.com>
2022-03-31 15:11:02 +0000

    refactor metadta mocking to be more generic

--- hi-ml-histopathology/testhisto/testhisto/utils/utils_slides_datamodule.py
+++ hi-ml-histopathology/testhisto/testhisto/utils/utils_slides_datamodule.py
@@ -2,12 +2,21 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
+import os
 import pandas as pd
+import numpy as np
 
 from typing import Tuple, Optional
 from histopathology.datamodules.base_module import SlidesDataModule
 from histopathology.datasets.base_dataset import SlidesDataset
 from health_azure.utils import PathOrString
+
+METADATA_POSSIBLE_VALUES = {
+    "data_provider": ["site_1", "site_2"],
+    "isup_grade": [0, 4, 1, 3, 0, 5, 2, 5, 5, 4, 4],
+    "gleason_score": ["0+0", "4+4", "3+3", "4+3", "negative", "4+5", "3+4", "5+4", "5+5", "5+3", "3+5"],
+}
+N_GLEASON_SCORES = 11
 
 
 class MockSlidesDataset(SlidesDataset):
@@ -18,15 +27,15 @@ class MockSlidesDataset(SlidesDataset):
     :param LABEL_COLUMN: CSV column name for tile label set to "isup_grade".
     :param METADATA_COLUMNS: Column names for all the metadata available on the CSV dataset file.
     """
+
     SLIDE_ID_COLUMN = "image_id"
     LABEL_COLUMN = "isup_grade"
 
     METADATA_COLUMNS = ("data_provider", "isup_grade", "gleason_score")
 
-    def __init__(self,
-                 root: PathOrString,
-                 dataset_csv: Optional[PathOrString] = None,
-                 dataset_df: Optional[pd.DataFrame] = None) -> None:
+    def __init__(
+        self, root: PathOrString, dataset_csv: Optional[PathOrString] = None, dataset_df: Optional[pd.DataFrame] = None
+    ) -> None:
         """
         :param root: Root directory of the dataset.
         :param dataset_csv: Full path to a dataset CSV file, containing at least
@@ -44,5 +53,24 @@ class MockSlidesDataset(SlidesDataset):
 class MockSlidesDataModule(SlidesDataModule):
     """Mock and child class of SlidesDataModule. It overrides get_splits so that it uses MockSlidesDataset.
     """
+
     def get_splits(self) -> Tuple[MockSlidesDataset, MockSlidesDataset, MockSlidesDataset]:
         return (MockSlidesDataset(self.root_path), MockSlidesDataset(self.root_path), MockSlidesDataset(self.root_path))
+
+
+def create_mock_metadata_dataframe(tmp_path: str, n_samples: int = 4, seed: int = 42) -> None:
+    """Create a mock dataframe with random metadata.
+
+    :param tmp_path: A temporary directory to store the mock CSV file.
+    :param n_samples: Number of random samples to generate, defaults to 4.
+    :param seed: pseudorandom number generator seed to use for mocking random metadata, defaults to 42.
+    """
+    np.random.seed(seed)
+    mock_metadata: dict = {col: [] for col in [MockSlidesDataset.IMAGE_COLUMN, *MockSlidesDataset.METADATA_COLUMNS]}
+    for i in range(n_samples):
+        rand_id = np.random.randint(0, N_GLEASON_SCORES)
+        mock_metadata[MockSlidesDataset.IMAGE_COLUMN].append(f"_{i}")
+        for key, val in METADATA_POSSIBLE_VALUES:
+            mock_metadata[key].append(val[rand_id if len(val) == N_GLEASON_SCORES else np.random.randint(2)])
+    df = pd.DataFrame(data=mock_metadata)
+    df.to_csv(os.path.join(tmp_path, MockSlidesDataset.DEFAULT_CSV_FILENAME), index=False)
