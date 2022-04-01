dae16b304532a8c667e39e9311fa1a9c167b9a30
Kenza <t-kbouzid@microsoft.com>
2022-04-01 14:53:36 +0000

    move wsi_utils to histo folder

--- hi-ml-histopathology/src/histopathology/utils/wsi_utils.py
+++ hi-ml-histopathology/src/histopathology/utils/wsi_utils.py
@@ -1,11 +1,12 @@
-from async_timeout import Any
 import torch
 import numpy as np
-from torch.utils.data.dataloader import default_collate
+
 from typing import List
+from histopathology.utils.naming import SlideKey
+from torch.utils.data.dataloader import default_collate
 
 
-def list_data_collate(batch: List) -> Any:
+def list_data_collate(batch: List[List[dict]]) -> List[dict]:
     """
         Combine instances from a list of dicts into a single dict, by stacking them along first dim
         [{'image' : 3xHxW}, {'image' : 3xHxW}, {'image' : 3xHxW}...] - > {'image' : Nx3xHxW}
@@ -14,6 +15,6 @@ def list_data_collate(batch: List) -> Any:
 
     for i, item in enumerate(batch):
         data = item[0]
-        data["image"] = torch.tensor(np.array([ix["image"] for ix in item]))
+        data[SlideKey.IMAGE] = torch.tensor(np.array([ix[SlideKey.IMAGE] for ix in item]))
         batch[i] = data
     return default_collate(batch)
