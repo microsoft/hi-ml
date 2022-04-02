c296e2af60d3bc8bdea00076d914ecd53a6e868d
Anton Schwaighofer <antonsc@microsoft.com>
2022-04-02 21:03:44 +0200

    code and tests

--- hi-ml/testhiml/conftest.py
+++ hi-ml/testhiml/conftest.py
@@ -4,8 +4,12 @@ from pathlib import Path
 
 
 root = Path(__file__).parent.parent.parent
-for folder in ["hi-ml-azure", "hi-ml"]:
-    full_folder = str(root / folder / "src")
+for folder in [
+    Path("hi-ml-azure") / "src",
+    Path("hi-ml-azure") / "testazure",
+    Path("hi-ml") / "src",
+]:
+    full_folder = str(root / folder)
     if full_folder not in sys.path:
         print(f"Adding to sys.path for running hi-ml: {full_folder}")
         sys.path.insert(0, full_folder)
