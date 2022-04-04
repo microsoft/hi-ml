056b33e6800c5f436877f4d37412c27fb6b87f7e
Kenza <t-kbouzid@microsoft.com>
2022-04-04 12:45:07 +0000

    doc string update

--- hi-ml-histopathology/src/histopathology/utils/wsi_utils.py
+++ hi-ml-histopathology/src/histopathology/utils/wsi_utils.py
@@ -10,7 +10,8 @@ def image_collate(batch: List[dict]) -> List[dict]:
     """
         Combine instances from a list of dicts into a single dict, by stacking them along first dim
         [{'image' : 3xHxW}, {'image' : 3xHxW}, {'image' : 3xHxW}...] - > {'image' : Nx3xHxW}
-        followed by the default collate which will form a batch BxNx3xHxW
+        followed by the default collate which will form a batch BxNx3xHxW.
+        The list of dicts refers to the the list of tiles produced by the TileOnGridd transform applied on a WSI.
     """
 
     for i, item in enumerate(batch):
