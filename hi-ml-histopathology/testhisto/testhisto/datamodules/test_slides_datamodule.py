3d11f1855267d2a79fa6604fa64f2cf5bc3f6a49
Kenza <t-kbouzid@microsoft.com>
2022-04-26 09:16:28 +0000

    fix imports

--- hi-ml-histopathology/testhisto/testhisto/datamodules/test_slides_datamodule.py
+++ hi-ml-histopathology/testhisto/testhisto/datamodules/test_slides_datamodule.py
@@ -2,7 +2,7 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
-
+import shutil
 from typing import Generator
 import pytest
 import logging
