1ddbf7c4f119e75cae055e14d689e163015edb4b
Kenza <t-kbouzid@microsoft.com>
2022-05-19 13:59:37 +0000

    fix module not found

--- hi-ml-histopathology/src/histopathology/scripts/mount_azure_dataset.py
+++ hi-ml-histopathology/src/histopathology/scripts/mount_azure_dataset.py
@@ -2,6 +2,13 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
+from pathlib import Path
+import sys
+
+himl_histo_root_dir = Path(__file__).parent.parent.parent
+himl_root = himl_histo_root_dir.parent.parent
+himl_azure_package_root = himl_root / "hi-ml-azure" / "src"
+sys.path.insert(0, str(himl_azure_package_root))
 
 from health_azure import DatasetConfig
 from health_azure.utils import get_workspace
@@ -21,7 +28,7 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     # Run this script as "python mount_azure_dataset.py --dataset_id TCGA-CRCk"
-    parser.add_argument('--dataset_id', type=str,
+    parser.add_argument('--dataset_id', type=str, default="PANDA",
                         help='Name of the Azure dataset e.g. PANDA or TCGA-CRCk')
     args = parser.parse_args()
     mount_dataset(args.dataset_id)
