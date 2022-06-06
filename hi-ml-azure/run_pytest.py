b96edada724bb0c77fdc6330ef0c99c3bfd80566
Kenza Bouzid <t-kbouzid@microsoft.com>
2022-06-06 12:45:44 +0000

    add coma

--- hi-ml-azure/run_pytest.py
+++ hi-ml-azure/run_pytest.py
@@ -81,7 +81,7 @@ def run_pytest(folder_to_test: str, pytest_mark: str, module_to_test: str) -> No
     pytest_args = [folder_to_test, f"--junitxml={str(results_file)}"]
 
     if module_to_test:
-        pytest_args += [f"--cov={module_to_test}", "--cov-branch", "--cov-report=html" "--cov-report=xml",
+        pytest_args += [f"--cov={module_to_test}", "--cov-branch", "--cov-report=html", "--cov-report=xml",
                         "--cov-report=term-missing", "--cov-config=.coveragerc"]
     if pytest_mark:
         pytest_args += ["-m", pytest_mark]
