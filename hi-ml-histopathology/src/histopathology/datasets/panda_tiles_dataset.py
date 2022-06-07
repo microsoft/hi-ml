6900ff94f750b327a5c652f2a0c233e16ca09c39
Harshita Sharma <61745616+harshita-s@users.noreply.github.com>
2022-06-07 11:52:31 +0100

    ENH: PANDA SSL with tile selection enabled (random or tile occupancy) (#398)
    In this PR,
    -Configuration for PANDA SSL is updated to run locally and on Azure ML.
    -Docs are updated to explain `occupancy_threshold` and `random_subset_fraction` parameters in `PandaTilesDataset`.
    -Parameter `random_subset_fraction` is added to `PandaTilesDataset` to select a random subset of tiles.

--- hi-ml-histopathology/src/histopathology/datasets/panda_tiles_dataset.py
+++ hi-ml-histopathology/src/histopathology/datasets/panda_tiles_dataset.py
@@ -38,17 +38,39 @@ class PandaTilesDataset(TilesDataset):
                  root: Path,
                  dataset_csv: Optional[Union[str, Path]] = None,
                  dataset_df: Optional[pd.DataFrame] = None,
-                 occupancy_threshold: Optional[float] = None) -> None:
+                 occupancy_threshold: Optional[float] = None,
+                 random_subset_fraction: Optional[float] = None) -> None:
+        """
+        :param root: Root directory of the dataset.
+        :param dataset_csv: Full path to a dataset CSV file, containing at least
+        `TILE_ID_COLUMN`, `SLIDE_ID_COLUMN`, and `IMAGE_COLUMN`. If omitted, the CSV will be read
+        from `"{root}/{DEFAULT_CSV_FILENAME}"`.
+        :param dataset_df: A potentially pre-processed dataframe in the same format as would be read
+        from the dataset CSV file, e.g. after some filtering. If given, overrides `dataset_csv`.
+        :occupancy_threshold: A value between 0-1 such that only tiles with occupancy > occupancy_threshold
+        will be selected. If 0, all tiles are selected. If `None` (default), all tiles are selected.
+        :random_subset_fraction: A value > 0 and <=1 such that this proportion of tiles will be randomly selected.
+        If 1, all tiles are selected. If `None` (default), all tiles are selected.
+        """
         super().__init__(root=Path(root) / self._RELATIVE_ROOT_FOLDER,
                          dataset_csv=dataset_csv,
                          dataset_df=dataset_df,
                          train=None,
                          validate_columns=False)
+
         if occupancy_threshold is not None:
-            self.dataset_df: pd.DataFrame
+            if (occupancy_threshold < 0) or (occupancy_threshold > 1):
+                raise ValueError(f"Occupancy threshold value {occupancy_threshold} should be in range 0-1.")
             dataset_df_filtered = self.dataset_df.loc[
                 self.dataset_df['occupancy'] > occupancy_threshold
             ]
+            self.dataset_df = dataset_df_filtered
+
+        if random_subset_fraction is not None:
+            if (random_subset_fraction <= 0) or (random_subset_fraction > 1):
+                raise ValueError(f"Random subset fraction value {random_subset_fraction} should be > 0 and < = 1.")
+            df_length_random_subset_fraction = round(len(self.dataset_df) * random_subset_fraction)
+            dataset_df_filtered = self.dataset_df.sample(n=df_length_random_subset_fraction)
             self.dataset_df = dataset_df_filtered
 
         # Copy columns "left" --> "tile_x" and "top" --> "tile_y"
@@ -66,6 +88,7 @@ class PandaTilesDatasetReturnImageLabel(VisionDataset):
     class label.
     """
     occupancy_threshold = 0
+    random_subset_fraction = 1
 
     def __init__(self,
                  root: Path,
@@ -78,7 +101,8 @@ class PandaTilesDatasetReturnImageLabel(VisionDataset):
         self.base_dataset = PandaTilesDataset(root=root,
                                               dataset_csv=dataset_csv,
                                               dataset_df=dataset_df,
-                                              occupancy_threshold=self.occupancy_threshold)
+                                              occupancy_threshold=self.occupancy_threshold,
+                                              random_subset_fraction=self.random_subset_fraction)
 
     def __getitem__(self, index: int) -> Tuple:  # type: ignore
         sample = self.base_dataset[index]
