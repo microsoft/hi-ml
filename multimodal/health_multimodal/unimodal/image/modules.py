29c13f6f62c5dd1e8f12c8e4f71e59f24d914721
Ozan Oktay <ozan.oktay@microsoft.com>
2022-06-13 19:59:07 +0000

    add file headers

--- multimodal/health_multimodal/unimodal/image/modules.py
+++ multimodal/health_multimodal/unimodal/image/modules.py
@@ -1,3 +1,9 @@
+#  -------------------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+#  -------------------------------------------------------------------------------------------
+
+
 import torch
 import torch.nn as nn
 
