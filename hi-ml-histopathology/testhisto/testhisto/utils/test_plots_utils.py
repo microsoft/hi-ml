584f19687d1b1c4fba6f6ce953c414841ab15ffa
Kenza Bouzid <t-kbouzid@microsoft.com>
2022-06-21 09:12:01 +0000

    use context managers for mocking functions

--- hi-ml-histopathology/testhisto/testhisto/utils/test_plots_utils.py
+++ hi-ml-histopathology/testhisto/testhisto/utils/test_plots_utils.py
@@ -44,30 +44,26 @@ def assert_plot_func_called_if_among_plot_options(
         },
     ],
 )
-@patch("histopathology.utils.plots_utils.save_confusion_matrix")
-@patch("histopathology.utils.plots_utils.save_scores_histogram")
-@patch("histopathology.utils.plots_utils.save_top_and_bottom_tiles")
-@patch("histopathology.utils.plots_utils.save_slide_thumbnail_and_heatmap")
 def test_plots_handler_plots_only_desired_plot_options(
-    mock_slide: MagicMock,
-    mock_tile: MagicMock,
-    mock_histogram: MagicMock,
-    mock_conf: MagicMock,
     plot_options: Collection[PlotOption],
 ) -> None:
     plots_handler = DeepMILPlotsHandler(plot_options, class_names=["foo"])
     plots_handler.slides_dataset = MagicMock()
 
     n_tiles = 4
-    # n_slides = 2
     slide_node = SlideNode(slide_id="1", prob_score=0.5, true_label=1, pred_label=0)
     tiles_selector = TilesSelector(n_classes=2, num_slides=4, num_tiles=2)
     tiles_selector.top_slides_heaps = {0: [slide_node] * n_tiles, 1: [slide_node] * n_tiles}
     tiles_selector.bottom_slides_heaps = {0: [slide_node] * n_tiles, 1: [slide_node] * n_tiles}
 
-    plots_handler.save_plots(
-        outputs_dir=MagicMock(), tiles_selector=tiles_selector, results=MagicMock(), stage=ModelKey.VAL
-    )
+    with patch("histopathology.utils.plots_utils.save_slide_thumbnail_and_heatmap") as mock_slide:
+        with patch("histopathology.utils.plots_utils.save_top_and_bottom_tiles") as mock_tile:
+            with patch("histopathology.utils.plots_utils.save_scores_histogram") as mock_histogram:
+                with patch("histopathology.utils.plots_utils.save_confusion_matrix") as mock_conf:
+                    plots_handler.save_plots(
+                        outputs_dir=MagicMock(), tiles_selector=tiles_selector, results=MagicMock(), stage=ModelKey.VAL
+                    )
+
     calls_count = 0
     calls_count += assert_plot_func_called_if_among_plot_options(
         mock_slide, PlotOption.SLIDE_THUMBNAIL_HEATMAP, plot_options
@@ -75,4 +71,5 @@ def test_plots_handler_plots_only_desired_plot_options(
     calls_count += assert_plot_func_called_if_among_plot_options(mock_tile, PlotOption.TOP_BOTTOM_TILES, plot_options)
     calls_count += assert_plot_func_called_if_among_plot_options(mock_histogram, PlotOption.HISTOGRAM, plot_options)
     calls_count += assert_plot_func_called_if_among_plot_options(mock_conf, PlotOption.CONFUSION_MATRIX, plot_options)
+
     assert calls_count == len(plot_options)
