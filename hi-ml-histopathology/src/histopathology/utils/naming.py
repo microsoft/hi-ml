fb30d6975a4bfe3f421a810f9f4a4519dd77dd3b
Kenza Bouzid <t-kbouzid@microsoft.com>
2022-06-21 08:52:22 +0000

    remove str from enum

--- hi-ml-histopathology/src/histopathology/utils/naming.py
+++ hi-ml-histopathology/src/histopathology/utils/naming.py
@@ -89,7 +89,7 @@ class AMLMetricsJsonKey(str, Enum):
     CLASS_NAMES = 'class_names'
 
 
-class PlotOption(str, Enum):
+class PlotOption(Enum):
     TOP_BOTTOM_TILES = "top_bottom_tiles"
     SLIDE_THUMBNAIL_HEATMAP = "slide_thumbnail_heatmap"
     CONFUSION_MATRIX = "confusion_matrix"
