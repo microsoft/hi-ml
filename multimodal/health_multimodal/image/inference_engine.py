fa6cdec2f7f7b0b978ec86f05a50200cc8890595
Fernando Pérez-García <fperezgarcia@microsoft.com>
2022-06-21 12:46:30 +0000

    Fix type of returned value

--- multimodal/health_multimodal/image/inference_engine.py
+++ multimodal/health_multimodal/image/inference_engine.py
@@ -5,7 +5,7 @@
 
 
 from pathlib import Path
-from typing import Tuple, Union
+from typing import Callable, Tuple
 
 import torch
 from torchvision.transforms import Compose
@@ -13,6 +13,8 @@ from torchvision.transforms import Compose
 from health_multimodal.image.data.io import load_image
 from health_multimodal.image.data.transforms import infer_resize_params
 from health_multimodal.image.model.model import ImageModel
+
+TypeShape2D = Tuple[int, int]
 
 
 class ImageInferenceEngine:
@@ -36,34 +38,29 @@ class ImageInferenceEngine:
         self.model.eval()
         self.resize_size, self.crop_size = infer_resize_params(self.transform.transforms)
 
-    def load_and_transform_input_image(self,
-                                       image_path: Path,
-                                       return_original_shape: bool = False) -> \
-            Union[torch.Tensor, Tuple[torch.Tensor, Tuple[int, int]]]:
-        """
+    def load_and_transform_input_image(self, image_path: Path, transform: Callable) -> Tuple[torch.Tensor, TypeShape2D]:
+        """Read an image and apply the transform to it.
+
         1. Read the image from the given path
-        2. Apply transforms
+        2. Apply transform
         3. Add the batch dimension
-        4. Move to the correct device.
+        4. Move to the correct device
 
         :param return_original_shape: Whether to return an extra tuple that has the original shape of the image
             before the transforms. The tuple returned contains (width, height).
         """
         image = load_image(image_path)
-        transformed_image = self.transform(image).unsqueeze(0).to(self.device)
-        if return_original_shape:
-            return transformed_image, image.size
-        else:
-            return transformed_image
+        transformed_image = transform(image).unsqueeze(0).to(self.device)
+        return transformed_image, image.size
 
     @torch.no_grad()
-    def get_patch_embeddings_from_image(self, image_path: Path) -> Tuple[torch.Tensor, Tuple[int, int]]:
-        """
-        Computes image embeddings in the joint latent space and preserves the image grid.
+    def get_patch_embeddings_from_image(self, image_path: Path) -> Tuple[torch.Tensor, TypeShape2D]:
+        """Compute image embeddings in the joint latent space, preserving the image grid.
+
         :param image_path: Path to the image to compute embeddings for.
         """
-        input_image, img_shape = self.load_and_transform_input_image(image_path, return_original_shape=True)
-        projected_img_emb = self.model.get_patchwise_projected_embeddings(input_image, normalize=True)  # type: ignore
+        input_image, img_shape = self.load_and_transform_input_image(image_path, self.transform)
+        projected_img_emb = self.model.get_patchwise_projected_embeddings(input_image, normalize=True)
         assert projected_img_emb.shape[0] == 1
 
         return projected_img_emb[0], img_shape
