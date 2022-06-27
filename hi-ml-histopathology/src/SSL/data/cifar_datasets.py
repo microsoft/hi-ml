976c2c4edb474d94a3694b4039df96f32ad87601
Kenza Bouzid <t-kbouzid@microsoft.com>
2022-06-27 08:34:40 +0000

    fixed cifar test

--- hi-ml-histopathology/src/SSL/data/cifar_datasets.py
+++ hi-ml-histopathology/src/SSL/data/cifar_datasets.py
@@ -3,7 +3,6 @@
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
 
-from typing import Any
 from torchvision.datasets import CIFAR10, CIFAR100
 
 from SSL.data.dataset_cls_utils import DataClassBaseWithReturnIndex
@@ -14,9 +13,6 @@ class HimlCifar10(DataClassBaseWithReturnIndex, CIFAR10):
     Wrapper class around torchvision CIFAR10 class to optionally return the
     index on top of the image and the label in __getitem__ as well as defining num_classes property.
     """
-    def __init__(self, root: str, return_index: bool, **kwargs: Any) -> None:
-        kwargs["download"] = True
-        super().__init__(root, return_index, **kwargs)
 
     @property
     def num_classes(self) -> int:
