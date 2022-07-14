b9d8bb754157a6dda70076c3cc8d404d005a67a3
Melissa Bristow <mebristo@microsoft.com>
2022-07-14 11:49:23 +0100

    config missing quotations

--- create_config.sh
+++ create_config.sh
@@ -1,8 +1,8 @@
 cat >config.json <<EOL
 {
-    subscription_id: \$HIML_SUBSCRIPTION_ID
-    resource_group: \$HIML_RESOURCE_GROUP
-    workspace_name: \$HIML_WORKSPACE_NAME
+    "subscription_id": "$HIML_SUBSCRIPTION_ID",
+    "resource_group": "$HIML_RESOURCE_GROUP",
+    "workspace_name": "$HIML_WORKSPACE_NAME"
 
 }
 
