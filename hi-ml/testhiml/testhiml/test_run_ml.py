d88d20cb5d44e41321ba3d2164bd90eace77ee50
Kenza Bouzid <t-kbouzid@microsoft.com>
2022-07-21 18:14:45 +0000

    flake8

--- hi-ml/testhiml/testhiml/test_run_ml.py
+++ hi-ml/testhiml/testhiml/test_run_ml.py
@@ -1,4 +1,7 @@
-import torch
+#  ------------------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+#  ------------------------------------------------------------------------------------------
 import shutil
 import pytest
 
