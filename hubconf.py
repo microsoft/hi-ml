63d90f56d2c87d4cfcafb2e5c67eeafb64e29c6b
Fernando Pérez-García <fperezgarcia@microsoft.com>
2022-07-28 08:12:40 +0100

    Add only the model folder to path

--- hubconf.py
+++ hubconf.py
@@ -4,11 +4,11 @@ dependencies = ["torch", "torchvision"]
 import sys
 from pathlib import Path
 repo_dir = Path(__file__).parent
-multimodal_src_dir = repo_dir / "hi-ml-multimodal" / "src" / "health_multimodal"
+multimodal_src_dir = repo_dir / "hi-ml-multimodal" / "src" / "health_multimodal" / "image"
 sys.path.append(str(multimodal_src_dir))
 
-from image import ImageModel
-from image import get_biovil_resnet as _biovil_resnet
+from model import ImageModel
+from model import get_biovil_resnet as _biovil_resnet
 # autopep8: on
 
 
