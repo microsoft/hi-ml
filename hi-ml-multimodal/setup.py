a3a40eb2c4c4b1b134367dd024a84bd078e22371
Fernando Pérez-García <fepegar@gmail.com>
2022-08-02 21:55:00 +0100

    Fix import

--- hi-ml-multimodal/setup.py
+++ hi-ml-multimodal/setup.py
@@ -3,7 +3,7 @@
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
 
-import pathlib
+from pathlib import Path
 from setuptools import find_namespace_packages, setup  # type: ignore
 
 
