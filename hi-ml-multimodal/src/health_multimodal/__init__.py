4fb3b6c96ae1e26e2dfb28800e66254eb9c68856
Fernando Pérez-García <fperezgarcia@microsoft.com>
2022-08-03 13:46:17 +0100

    Add version number to package __init__

--- hi-ml-multimodal/src/health_multimodal/__init__.py
+++ hi-ml-multimodal/src/health_multimodal/__init__.py
@@ -2,3 +2,5 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  -------------------------------------------------------------------------------------------
+
+__version__ = "0.1.0"
