ce1224077b3e3ea80ba0f3f5767f47465cf76b21
Kenza Bouzid <37396332+kenza-bouzid@users.noreply.github.com>
2022-08-11 17:18:34 +0100

    BUG: Set dataframe index column only when necessary (#569)
    Index datasets dataframe by tile_id or slide_id only when necessary (e.g. the current index is not already to tile_id or slide_id columns)
    * update check

--- hi-ml-cpath/src/health_cpath/datasets/base_dataset.py
+++ hi-ml-cpath/src/health_cpath/datasets/base_dataset.py
@@ -81,7 +81,8 @@ class TilesDataset(Dataset):
             self.dataset_csv = dataset_csv or self.root_dir / self.DEFAULT_CSV_FILENAME
             dataset_df = pd.read_csv(self.dataset_csv, **dataframe_kwargs)
 
-        dataset_df = dataset_df.set_index(self.TILE_ID_COLUMN)
+        if dataset_df.index.name != self.TILE_ID_COLUMN:
+            dataset_df = dataset_df.set_index(self.TILE_ID_COLUMN)
         if train is None:
             self.dataset_df = dataset_df
         else:
@@ -191,7 +192,8 @@ class SlidesDataset(Dataset):
             self.dataset_csv = dataset_csv or self.root_dir / self.DEFAULT_CSV_FILENAME
             dataset_df = pd.read_csv(self.dataset_csv, **dataframe_kwargs)
 
-        dataset_df = dataset_df.set_index(self.SLIDE_ID_COLUMN)
+        if dataset_df.index.name != self.SLIDE_ID_COLUMN:
+            dataset_df = dataset_df.set_index(self.SLIDE_ID_COLUMN)
         if train is None:
             self.dataset_df = dataset_df
         else:
