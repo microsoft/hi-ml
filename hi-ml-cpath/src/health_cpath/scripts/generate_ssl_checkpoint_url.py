54d1e6e858a2a2e354ec0247958a1691a1b941b4
Kenza Bouzid <t-kbouzid@microsoft.com>
2022-10-26 16:43:13 +0000

    add script to generate ssl url

---
+++ hi-ml-cpath/src/health_cpath/scripts/generate_ssl_checkpoint_url.py
@@ -0,0 +1,44 @@
+from azure.storage.blob import generate_blob_sas, BlobSasPermissions
+from datetime import datetime, timedelta
+from health_azure import get_workspace
+
+
+def generate_ssl_checkpoint_url(run_id: str, checkpoint_filename: str, expiry_hours: int = 1) -> str:
+    """Generate a SAS URL for the SSL checkpoint file in the given run.
+
+    :param run_id: The run ID of the SSL checkpoint.
+    :param checkpoint_filename: The filename of the SSL checkpoint.
+    :param expiry_hours: The number of hours the SAS URL is valid for, defaults to 1.
+    :return: The SAS URL for the SSL checkpoint.
+    """
+    datastore = get_workspace().get_default_datastore()
+    account_name = datastore.account_name
+    container_name = 'azureml'
+    blob_name = f'ExperimentRun/dcid.{run_id}/{checkpoint_filename}'
+
+    sas_token = generate_blob_sas(account_name=datastore.account_name,
+                                  container_name=container_name,
+                                  blob_name=blob_name,
+                                  account_key=datastore.account_key,
+                                  permission=BlobSasPermissions(read=True),
+                                  expiry=datetime.utcnow() + timedelta(hours=expiry_hours))
+
+    return f'https://{account_name}.blob.core.windows.net/{container_name}/{blob_name}?{sas_token}'
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--run_id', type=str, help='The run id of the SSL model checkpoint',
+                        default='1f391509-f0a7-41d9-bef5-06713739fb0b')
+    parser.add_argument(
+        '--checkpoint_filename',
+        type=str,
+        default='last.ckpt',
+        help='The filename of the SSL model checkpoint. Default: last.ckpt')
+    parser.add_argument(
+        '--expiry_hours', type=int, default=1, help='The number of hours for which the SAS token is valid. Default: 1'
+    )
+    args = parser.parse_args()
+    ssl_url = generate_ssl_checkpoint_url(args.run_id, args.checkpoint_filename, args.expiry_hours)
+    print(f'SSL checkpoint URL: {ssl_url}')
