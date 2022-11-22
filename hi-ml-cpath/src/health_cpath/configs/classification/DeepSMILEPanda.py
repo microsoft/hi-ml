a370d1fda8823e0aba2dee02a460d926849603f3
Kenza Bouzid <t-kbouzid@microsoft.com>
2022-11-22 11:15:16 +0000

    wip non regression

--- hi-ml-cpath/src/health_cpath/configs/classification/DeepSMILEPanda.py
+++ hi-ml-cpath/src/health_cpath/configs/classification/DeepSMILEPanda.py
@@ -40,8 +40,7 @@ class BaseDeepSMILEPanda(BaseMIL):
             adam_betas=(0.9, 0.99),
             # loading params:
             backend=WSIBackend.CUCIM,
-            roi_type=ROIType.FOREGROUND,
-            foreground_threshold=255,
+            roi_type=ROIType.MASK,
             margin=0,
             image_key=SlideKey.IMAGE,
             mask_key=SlideKey.MASK,
