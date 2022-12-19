9598e902fdad48dce4529f6ccc200a5580e1d4e6
Anton Schwaighofer <antonsc@microsoft.com>
2022-12-19 05:13:06 -0800

    handle exceptions when cancelling

--- .github/actions/cancel_azureml_jobs/cancel_aml_jobs.py
+++ .github/actions/cancel_azureml_jobs/cancel_aml_jobs.py
@@ -36,7 +36,10 @@ def cancel_running_and_queued_jobs() -> None:
             print(f"Skipping {status_suffix}")
         else:
             print(f"Cancelling {status_suffix}")
-            run.cancel()
+            try:
+                run.cancel()
+            except Exception as ex:
+                print(f"Unable to cancel {status_suffix}: {ex}")
 
 
 if __name__ == "__main__":
