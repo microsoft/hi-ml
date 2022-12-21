5d9aeb17a7f7e606111ff8fd05149dbbe6e46965
Peter Hessey <a-phessey@microsoft.com>
2022-12-21 16:24:23 +0000

    🔥 Undo accidental change

--- hi-ml-azure/src/health_azure/examples/elevate_this.py
+++ hi-ml-azure/src/health_azure/examples/elevate_this.py
@@ -26,8 +26,7 @@ def main() -> None:
     _ = submit_to_azure_if_needed(
         compute_cluster_name="lite-testing-ds2",
         wait_for_completion=True,
-        wait_for_completion_show_output=True,
-        strictly_aml_v1=True)
+        wait_for_completion_show_output=True)
 
     parser = ArgumentParser()
     parser.add_argument("-m", "--message", type=str, required=True, help="The message to print out")
