b8270bafb253aa64d99a597602a0ad7bcbef1046
Kenza Bouzid <t-kbouzid@microsoft.com>
2023-01-09 14:39:50 +0000

    flake8

--- hi-ml-cpath/testSSL/conftest.py
+++ hi-ml-cpath/testSSL/conftest.py
@@ -4,7 +4,6 @@ Global PyTest configuration -- used to define global fixtures for the entire tes
 DO NOT RENAME THIS FILE: (https://docs.pytest.org/en/latest/fixture.html#sharing-a-fixture-across-tests-in-a-module
 -or-class-session)
 """
-import logging
 import shutil
 import sys
 import uuid
