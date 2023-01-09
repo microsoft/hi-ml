5c49b74a28cf99dfe283f944d8f43a97acdb94f6
Mark <rmappin@ucl.ac.uk>
2023-01-09 11:48:06 +0000

    Return x0 in addition to x1-4

--- hi-ml-multimodal/src/health_multimodal/image/model/resnet.py
+++ hi-ml-multimodal/src/health_multimodal/image/model/resnet.py
@@ -34,7 +34,7 @@ class ResNetHIML(ResNet):
         x4 = self.layer4(x3)
 
         if return_skip:
-            return x1, x2, x3, x4
+            return x0, x1, x2, x3, x4
         else:
             return x4
 
