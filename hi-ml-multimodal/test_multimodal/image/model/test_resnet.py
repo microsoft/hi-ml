995825ff034743fa4eda11036e328abebf5737a7
Fernando Pérez-García <fepegar@gmail.com>
2023-01-13 11:28:47 +0000

    Minor edits to improve readability

--- hi-ml-multimodal/test_multimodal/image/model/test_resnet.py
+++ hi-ml-multimodal/test_multimodal/image/model/test_resnet.py
@@ -10,31 +10,27 @@ from health_multimodal.image.model.resnet import resnet18, resnet50
 
 @torch.no_grad()
 def test_return_intermediate_layers() -> None:
-    """
-    Checks if the ``return_intermediate_layers`` argument works correctly
-    """
+    """Check if the ``return_intermediate_layers`` keyword argument works correctly."""
 
     image = torch.rand(1, 3, 480, 480)
-    N, C, H, W = 1, 64, 120, 120
-    x0_dims = (N, C, H, W)
+    x0_dims = batch_size, num_channels, height, width = 1, 64, 120, 120
 
     # Check resnet18 intermediate layers
     model = resnet18(pretrained=False, progress=False)
     outputs = model(image, return_intermediate_layers=True)
     assert outputs[0].shape == x0_dims
 
-    layer_dims = (N, C, H, W)
+    layer_dims = batch_size, num_channels, height, width
     for i in range(1, len(outputs)):
         assert outputs[i].shape == layer_dims
-        layer_dims = (N, layer_dims[1] * 2, layer_dims[2] // 2, layer_dims[3] // 2)
+        layer_dims = batch_size, layer_dims[1] * 2, layer_dims[2] // 2, layer_dims[3] // 2
 
     # Check resnet50 intermediate layers
     model = resnet50(pretrained=False, progress=False)
     outputs = model(image, return_intermediate_layers=True)
     assert outputs[0].shape == x0_dims
 
-    expansion = 4
-    layer_dims = (N, C * expansion, H, W)
+    layer_dims = batch_size, 4 * num_channels, height, width
     for i in range(1, len(outputs)):
         assert outputs[i].shape == layer_dims
-        layer_dims = (N, layer_dims[1] * 2, layer_dims[2] // 2, layer_dims[3] // 2)
+        layer_dims = batch_size, layer_dims[1] * 2, layer_dims[2] // 2, layer_dims[3] // 2
