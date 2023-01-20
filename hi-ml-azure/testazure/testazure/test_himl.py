microsoft/hi-ml

commit eed89d7ef02d9c7b49ef570ac7a596c2830f1b41

ENH: Allow for distributed jobs in AML SDK v2 (#730)
Allow for distributed jobs in AML SDK v2

hi-ml-azure/src/health_azure/himl.py
@@ -469,7 +469,10 @@ def submit_run_v2(workspace: Optional[Workspace],
                   wait_for_completion_show_output: bool = False,
                   workspace_config_path: Optional[PathOrString] = None,
                   ml_client: Optional[MLClient] = None,
-                  hyperparam_args: Optional[Dict[str, Any]] = None) -> Job:
+                  hyperparam_args: Optional[Dict[str, Any]] = None,
+                  num_nodes: int = 1,
+                  processes_per_node: int = 1
+                  ) -> Job:
     """
     Starts a v2 AML Job on a given workspace by submitting a command
 
@@ -496,6 +499,8 @@ def submit_run_v2(workspace: Optional[Workspace],
         config
     :param ml_client: An Azure MLClient object for interacting with Azure resources.
     :param hyperparam_args: A dictionary of hyperparameter search args to pass into a sweep job.
+    :param num_nodes: The number of nodes to use for the job in AzureML.
+    :param processes_per_node: The number of processes per node to use for the job in AzureML.
     :return: An AzureML Run object.
     """
     if ml_client is None:
@@ -534,6 +539,10 @@ def submit_run_v2(workspace: Optional[Workspace],
     job_to_submit: Union[Command, Sweep]
     display_name = get_display_name_v2(tags)
 
+    # number of nodes and processes per node cannot be less than one
+    num_nodes = num_nodes if num_nodes >= 1 else 1
+    processes_per_node = processes_per_node if processes_per_node >= 1 else 1
+
     if hyperparam_args:
         param_sampling = hyperparam_args[PARAM_SAMPLING_ARG]
 
@@ -552,9 +561,11 @@ def submit_run_v2(workspace: Optional[Workspace],
             tags=tags or {},
             shm_size=docker_shm_size,
             display_name=display_name,
-            environment_variables={
-                "JOB_EXECUTION_MODE": "Basic",
-            }
+            instance_count=num_nodes,
+            distribution={
+                "type": "PyTorch",
+                "process_count_per_instance": processes_per_node,
+            },
         )
 
         del hyperparam_args[PARAM_SAMPLING_ARG]
@@ -585,9 +596,11 @@ def submit_run_v2(workspace: Optional[Workspace],
             tags=tags or {},
             shm_size=docker_shm_size,
             display_name=display_name,
-            environment_variables={
-                "JOB_EXECUTION_MODE": "Basic",
-            }
+            instance_count=num_nodes,
+            distribution={
+                "type": "PyTorch",
+                "process_count_per_instance": processes_per_node,
+            },
         )
 
     returned_job = ml_client.jobs.create_or_update(job_to_submit)
@@ -764,6 +777,7 @@ def submit_to_azure_if_needed(  # type: ignore
         hyperdrive_config: Optional[HyperDriveConfig] = None,
         hyperparam_args: Optional[Dict[str, Any]] = None,
         strictly_aml_v1: bool = False,
+        processes_per_node_v2: int = 1,
 ) -> AzureRunInfo:  # pragma: no cover
     """
     Submit a folder to Azure, if needed and run it.
@@ -822,6 +836,7 @@ def submit_to_azure_if_needed(  # type: ignore
         will be triggered if the commandline flag '--azureml' is present in sys.argv
     :param hyperdrive_config: A configuration object for Hyperdrive (hyperparameter search).
     :param strictly_aml_v1: If True, use Azure ML SDK v1. Otherwise, attempt to use Azure ML SDK v2.
+    :param processes_per_node_v2: The number of processes per node to use in distributed training on AzureML SDK v2.
     :return: If the script is submitted to AzureML then we terminate python as the script should be executed in AzureML,
         otherwise we return a AzureRunInfo object.
     """
@@ -965,7 +980,9 @@ def submit_to_azure_if_needed(  # type: ignore
                                 docker_shm_size=docker_shm_size,
                                 wait_for_completion=wait_for_completion,
                                 wait_for_completion_show_output=wait_for_completion_show_output,
-                                hyperparam_args=hyperparam_args
+                                hyperparam_args=hyperparam_args,
+                                num_nodes=num_nodes,
+                                processes_per_node=processes_per_node_v2,
                                 )
             if after_submission is not None:
                 after_submission(job, ml_client)  # type: ignore

hi-ml/src/health_ml/runner.py
@@ -267,6 +267,7 @@ class Runner:
                 after_submission=after_submission_hook,
                 tags=self.additional_run_tags(script_params),
                 strictly_aml_v1=self.experiment_config.strictly_aml_v1,
+                processes_per_node_v2=self.lightning_container.num_gpus_per_node(),
             )
         else:
             azure_run_info = submit_to_azure_if_needed(

commit e34d8999b2d553af66de76d249e59b60a9f55676

BUG: Training HelloWorld using AML v2 SDK hangs (#741)
This PR fixes a bug introduced in #730. It also adds smoke tests to run
training of the `HelloWorld` model in AML, using v1 and v2 SDK, using 1
or 2 nodes.
Closes #739

.github/actions/prepare_himl_python_env/action.yml
@@ -1,0 +1,30 @@
+name: 'HI-ML Python environment setup'
+description: 'Set up a Python environment for HI-ML and install packages in editable mode'
+inputs:
+  python-version:
+    description: 'The version of Python to use'
+    required: true
+
+runs:
+  using: "composite"
+  steps:
+    - name: Set up Python ${{ inputs.python-version }}
+      uses: actions/setup-python@v4
+      with:
+        python-version: ${{ inputs.python-version }}
+
+    - name: Install hi-ml-azure in editable mode
+      shell: bash
+      run: |
+        cd hi-ml-azure
+
+        # Install local package in editable mode
+        make pip_local
+
+    - name: Install hi-ml in editable mode
+      shell: bash
+      run: |
+        cd hi-ml
+
+        # Install local package in editable mode
+        make pip_local

.github/workflows/hi-ml-pr.yml
@@ -224,9 +224,96 @@ jobs:
         with:
             flags: ${{ matrix.folder }}
 
+  himl_smoke_helloworld_v1:
+    runs-on: ubuntu-20.04
+    needs: [ cancel-azureml ]
+    steps:
+      - uses: actions/checkout@v3
+        with:
+          lfs: true
+
+      - name: Set up Python ${{ env.pythonVersion }}
+        uses: ./.github/actions/prepare_himl_python_env
+        with:
+          python-version: ${{ env.pythonVersion }}
+
+      - name: Run smoke_helloworld_v1
+        run: |
+          cd hi-ml
+          make smoke_helloworld_v1
+
+  himl_smoke_helloworld_v2:
+    runs-on: ubuntu-20.04
+    needs: [ cancel-azureml ]
+    steps:
+      - uses: actions/checkout@v3
+        with:
+          lfs: true
+
+      - name: Set up Python ${{ env.pythonVersion }}
+        uses: ./.github/actions/prepare_himl_python_env
+        with:
+          python-version: ${{ env.pythonVersion }}
+
+      - name: Run smoke_helloworld_v2
+        run: |
+          cd hi-ml
+          make smoke_helloworld_v2
+
+  himl_smoke_helloworld_v1_2nodes:
+    runs-on: ubuntu-20.04
+    needs: [ cancel-azureml ]
+    steps:
+      - uses: actions/checkout@v3
+        with:
+          lfs: true
+
+      - name: Set up Python ${{ env.pythonVersion }}
+        uses: ./.github/actions/prepare_himl_python_env
+        with:
+          python-version: ${{ env.pythonVersion }}
+
+      - name: Run smoke_helloworld_v1_2nodes
+        run: |
+          cd hi-ml
+          make smoke_helloworld_v1_2nodes
+
+  himl_smoke_helloworld_v2_2nodes:
+    runs-on: ubuntu-20.04
+    needs: [ cancel-azureml ]
+    steps:
+      - uses: actions/checkout@v3
+        with:
+          lfs: true
+
+      - name: Set up Python ${{ env.pythonVersion }}
+        uses: ./.github/actions/prepare_himl_python_env
+        with:
+          python-version: ${{ env.pythonVersion }}
+
+      - name: Run smoke_helloworld_v2_2nodes
+        run: |
+          cd hi-ml
+          make smoke_helloworld_v2_2nodes
+
+  himl-smoke-tests-completed:
+    # This job is just a placeholder to ensure that all smoke tests have completed before
+    # publishing the package. Reference this job rather than the individual smoke tests.
+    runs-on: ubuntu-20.04
+    needs: [
+         himl_smoke_helloworld_v1,
+         himl_smoke_helloworld_v2,
+         himl_smoke_helloworld_v1_2nodes,
+         himl_smoke_helloworld_v2_2nodes,
+      ]
+    steps:
+      - name: Smoke tests completed
+        run: |
+          echo "All hi-ml smoke tests have passed"
+
   himl-publish-testpypi-pkg:
     runs-on: ubuntu-20.04
-    needs: [ himl-test-artifact-pkg, himl-pyright, himl-mypy, himl-flake8 ]
+    needs: [ himl-test-artifact-pkg, himl-pyright, himl-mypy, himl-flake8, himl-smoke-tests-completed ]
     if: "!startsWith(github.ref, 'refs/tags/v')"
     strategy:
       matrix:
@@ -258,7 +345,7 @@ jobs:
 
   himl-publish-pypi-pkg:
     runs-on: ubuntu-20.04
-    needs: [ himl-test-artifact-pkg, himl-pyright, himl-mypy, himl-flake8 ]
+    needs: [ himl-test-artifact-pkg, himl-pyright, himl-mypy, himl-flake8, himl-smoke-tests-completed ]
     strategy:
       matrix:
         folder: [ hi-ml, hi-ml-azure ]

hi-ml-azure/src/health_azure/himl.py
@@ -23,6 +23,7 @@ from azure.ai.ml import MLClient, Input, Output, command
 from azure.ai.ml.constants import AssetTypes, InputOutputModes
 from azure.ai.ml.entities import Data, Job, Command, Sweep
 from azure.ai.ml.entities import Environment as EnvironmentV2
+from azure.ai.ml.entities._job.distribution import MpiDistribution, PyTorchDistribution
 
 from azure.ai.ml.sweep import Choice
 from azureml._base_sdk_common import user_agent
@@ -466,12 +467,11 @@ def submit_run_v2(workspace: Optional[Workspace],
                   tags: Optional[Dict[str, str]] = None,
                   docker_shm_size: str = "",
                   wait_for_completion: bool = False,
-                  wait_for_completion_show_output: bool = False,
                   workspace_config_path: Optional[PathOrString] = None,
                   ml_client: Optional[MLClient] = None,
                   hyperparam_args: Optional[Dict[str, Any]] = None,
                   num_nodes: int = 1,
-                  processes_per_node: int = 1
+                  pytorch_processes_per_node: Optional[int] = None,
                   ) -> Job:
     """
     Starts a v2 AML Job on a given workspace by submitting a command
@@ -493,14 +493,14 @@ def submit_run_v2(workspace: Optional[Workspace],
     :param docker_shm_size: The Docker shared memory size that should be used when creating a new Docker image.
     :param wait_for_completion: If False (the default) return after the run is submitted to AzureML, otherwise wait for
         the completion of this run (if True).
-    :param wait_for_completion_show_output: If wait_for_completion is True this parameter indicates whether to show the
-        run output on sys.stdout.
     :param workspace_config_path: If not provided with an AzureML Workspace, then load one given the information in this
         config
     :param ml_client: An Azure MLClient object for interacting with Azure resources.
     :param hyperparam_args: A dictionary of hyperparameter search args to pass into a sweep job.
-    :param num_nodes: The number of nodes to use for the job in AzureML.
-    :param processes_per_node: The number of processes per node to use for the job in AzureML.
+    :param num_nodes: The number of nodes to use for the job in AzureML. The value must be 1 or greater.
+    :param pytorch_processes_per_node: For plain PyTorch multi-GPU processing: The number of processes per node.
+        If supplied, it will run a command job with the "pytorch" framework (rather than "Python"), and using "nccl"
+        as the communication backend.
     :return: An AzureML Run object.
     """
     if ml_client is None:
@@ -538,17 +538,24 @@ def submit_run_v2(workspace: Optional[Workspace],
     display_name = get_display_name_v2(tags)
 
     # number of nodes and processes per node cannot be less than one
+    if num_nodes < 1:
+        raise ValueError("num_nodes must be >= 1")
     num_nodes = num_nodes if num_nodes >= 1 else 1
-    processes_per_node = processes_per_node if processes_per_node >= 1 else 1
+    if pytorch_processes_per_node is not None:
+        if pytorch_processes_per_node < 1:
+            raise ValueError("pytorch_processes_per_node must be >= 1")
 
-    if hyperparam_args:
-        param_sampling = hyperparam_args[PARAM_SAMPLING_ARG]
-
-        for sample_param, choices in param_sampling.items():
-            input_datasets_v2[sample_param] = choices.values[0]
-            cmd += f" --{sample_param}=" + "${{inputs." + sample_param + "}}"
-
-        command_job = command(
+    def create_command_job(cmd: str) -> Command:
+        if pytorch_processes_per_node is None:
+            if num_nodes > 1:
+                distribution: Any = MpiDistribution(process_count_per_instance=1)
+            else:
+                # An empty dictionary for single node jobs would be in line with the type annotations on the
+                # 'command' function, but this is not recognized by the SDK. So we need to pass None instead.
+                distribution = None
+        else:
+            distribution = PyTorchDistribution(process_count_per_instance=pytorch_processes_per_node)
+        return command(
             code=str(snapshot_root_directory),
             command=cmd,
             inputs=input_datasets_v2,
@@ -560,11 +567,17 @@ def submit_run_v2(workspace: Optional[Workspace],
             shm_size=docker_shm_size,
             display_name=display_name,
             instance_count=num_nodes,
-            distribution={
-                "type": "PyTorch",
-                "process_count_per_instance": processes_per_node,
-            },
+            distribution=distribution,  # type: ignore
         )
+
+    if hyperparam_args:
+        param_sampling = hyperparam_args[PARAM_SAMPLING_ARG]
+
+        for sample_param, choices in param_sampling.items():
+            input_datasets_v2[sample_param] = choices.values[0]
+            cmd += f" --{sample_param}=" + "${{inputs." + sample_param + "}}"
+
+        command_job = create_command_job(cmd)
 
         del hyperparam_args[PARAM_SAMPLING_ARG]
         # override command with parameter expressions
@@ -583,23 +596,7 @@ def submit_run_v2(workspace: Optional[Workspace],
         job_to_submit.set_limits(max_total_trials=hyperparam_args.get(MAX_TOTAL_TRIALS_ARG, None))
 
     else:
-        job_to_submit = command(
-            code=str(snapshot_root_directory),
-            command=cmd,
-            inputs=input_datasets_v2,
-            outputs=output_datasets_v2,
-            environment=environment.name + "@latest",
-            compute=compute_target,
-            experiment_name=experiment_name,
-            tags=tags or {},
-            shm_size=docker_shm_size,
-            display_name=display_name,
-            instance_count=num_nodes,
-            distribution={
-                "type": "PyTorch",
-                "process_count_per_instance": processes_per_node,
-            },
-        )
+        job_to_submit = create_command_job(cmd)
 
     returned_job = ml_client.jobs.create_or_update(job_to_submit)
     logging.info(f"URL to job: {returned_job.services['Studio'].endpoint}")  # type: ignore
@@ -775,7 +772,7 @@ def submit_to_azure_if_needed(  # type: ignore
         hyperdrive_config: Optional[HyperDriveConfig] = None,
         hyperparam_args: Optional[Dict[str, Any]] = None,
         strictly_aml_v1: bool = False,
-        processes_per_node_v2: int = 1,
+        pytorch_processes_per_node_v2: Optional[int] = None,
 ) -> AzureRunInfo:  # pragma: no cover
     """
     Submit a folder to Azure, if needed and run it.
@@ -824,7 +821,13 @@ def submit_to_azure_if_needed(  # type: ignore
         will also register the data in this folder as an AzureML dataset.
     :param output_datasets: The script will create a temporary folder when running in AzureML, and while the job writes
         data to that folder, upload it to blob storage, in the data store.
-    :param num_nodes: The number of nodes to use in distributed training on AzureML.
+    :param num_nodes: The number of nodes to use in distributed training on AzureML. When using a value > 1, multiple
+        nodes in AzureML will be started. If `pytorch_processes_per_node_v2=None`, the job will be submitted
+        as a multi-node MPI job, with 1 process per node. This is suitable for PyTorch Lightning jobs.
+        If `pytorch_processes_per_node_v2` is not None,
+        a job with framework "PyTorch" and communication backend "nccl" will be started.
+        `pytorch_processes_per_node_v2` will guide the number of processes per node. This is suitable for plain PyTorch
+        training jobs without the use of frameworks like PyTorch Lightning.
     :param wait_for_completion: If False (the default) return after the run is submitted to AzureML, otherwise wait for
         the completion of this run (if True).
     :param wait_for_completion_show_output: If wait_for_completion is True this parameter indicates whether to show the
@@ -834,7 +837,9 @@ def submit_to_azure_if_needed(  # type: ignore
         will be triggered if the commandline flag '--azureml' is present in sys.argv
     :param hyperdrive_config: A configuration object for Hyperdrive (hyperparameter search).
     :param strictly_aml_v1: If True, use Azure ML SDK v1. Otherwise, attempt to use Azure ML SDK v2.
-    :param processes_per_node_v2: The number of processes per node to use in distributed training on AzureML SDK v2.
+    :param pytorch_processes_per_node_v2: For plain PyTorch multi-GPU processing: The number of processes per node. This
+        is only supported with AML SDK v2, and ignored in v1. If supplied, the job will be submitted as using the
+        "pytorch" framework (rather than "Python"), and using "nccl" as the communication backend.
     :return: If the script is submitted to AzureML then we terminate python as the script should be executed in AzureML,
         otherwise we return a AzureRunInfo object.
     """
@@ -977,10 +982,9 @@ def submit_to_azure_if_needed(  # type: ignore
                                 tags=tags,
                                 docker_shm_size=docker_shm_size,
                                 wait_for_completion=wait_for_completion,
-                                wait_for_completion_show_output=wait_for_completion_show_output,
                                 hyperparam_args=hyperparam_args,
                                 num_nodes=num_nodes,
-                                processes_per_node=processes_per_node_v2,
+                                pytorch_processes_per_node=pytorch_processes_per_node_v2,
                                 )
             if after_submission is not None:
                 after_submission(job, ml_client)  # type: ignore

hi-ml/Makefile
@@ -75,3 +75,26 @@ pytest_and_coverage: pip_test call_pytest_and_coverage
 
 # install test requirements and run all tests
 test_all: pip_test flake8 mypy call_pytest_and_coverage
+
+# Smoke tests should all wait for completion and run for 15 minutes maximum.
+# Tests run with the cpath Conda environment because of some odd problems with the hi-ml environment on GPU machines.
+# Some of that can be fixed by pinning torch==1.10.0, but then that triggers problems with distutils.
+define SHARED_ARGS
+--conda_env ../hi-ml-cpath/environment.yml --cluster=pr-gpu --max_run_duration=15m --wait_for_completion
+endef
+
+# HelloWorld model training on a single node, submitted using the v1 SDK
+smoke_helloworld_v1:
+	python src/health_ml/runner.py --model=health_ml.HelloWorld --strictly_aml_v1=True --tag smoke_helloworld_v1 ${SHARED_ARGS}
+
+# HelloWorld model training on a single node, submitted using the v2 SDK
+smoke_helloworld_v2:
+	python src/health_ml/runner.py --model=health_ml.HelloWorld --strictly_aml_v1=False --tag smoke_helloworld_v2 ${SHARED_ARGS}
+
+# HelloWorld model training on 2 nodes, submitted using the v1 SDK
+smoke_helloworld_v1_2nodes:
+	python src/health_ml/runner.py --model=health_ml.HelloWorld --strictly_aml_v1=True --num_nodes=2 --tag smoke_helloworld_v1_2nodes ${SHARED_ARGS}
+
+# HelloWorld model training on 2 nodes, submitted using the v2 SDK
+smoke_helloworld_v2_2nodes:
+	python src/health_ml/runner.py --model=health_ml.HelloWorld --strictly_aml_v1=False --num_nodes=2 --tag smoke_helloworld_v2_2nodes ${SHARED_ARGS}

hi-ml/primary_deps.yml
@@ -2,13 +2,14 @@
 # testing
 name: himl
 channels:
-  - defaults
   - pytorch
+  - conda-forge
+  - defaults
 dependencies:
+  - cudatoolkit=11.3.1
   - pip=20.1.1
   - python=3.7.3
   - pytorch=1.10.0
-  - cudatoolkit=11.3.1
   - pip:
       - -r ../hi-ml-azure/run_requirements.txt
       - -r run_requirements.txt

hi-ml/src/health_ml/runner.py
@@ -268,7 +268,6 @@ class Runner:
                 after_submission=after_submission_hook,
                 tags=self.additional_run_tags(script_params),
                 strictly_aml_v1=self.experiment_config.strictly_aml_v1,
-                processes_per_node_v2=self.lightning_container.num_gpus_per_node(),
             )
         else:
             azure_run_info = submit_to_azure_if_needed(

hi-ml-azure/testazure/testazure/test_himl.py
@@ -26,6 +26,7 @@ from _pytest.capture import CaptureFixture
 from azure.ai.ml import Input, Output, MLClient
 from azure.ai.ml.constants import AssetTypes, InputOutputModes
 from azure.ai.ml.entities import Data, Job
+from azure.ai.ml.entities._job.distribution import MpiDistribution, PyTorchDistribution
 from azure.ai.ml.sweep import Choice
 from azureml._restclient.constants import RunStatus
 from azureml.core import ComputeTarget, Environment, RunConfiguration, ScriptRunConfig, Workspace
@@ -797,7 +798,7 @@ def test_submit_run_v2(tmp_path: Path) -> None:
                 tags=dummy_tags,
                 shm_size=dummy_docker_shm_size,
                 display_name=dummy_display_name,
-                distribution={"type": "PyTorch", "process_count_per_instance": 1},
+                distribution=None,
                 instance_count=1
             )
 
@@ -857,7 +858,7 @@ def test_submit_run_v2(tmp_path: Path) -> None:
                 tags=dummy_tags,
                 shm_size=dummy_docker_shm_size,
                 display_name=dummy_display_name,
-                distribution={"type": "PyTorch", "process_count_per_instance": 1},
+                distribution=None,
                 instance_count=1
             )
 
@@ -1793,7 +1794,6 @@ def test_submit_to_azure_v2_distributed() -> None:
                 # If num_nodes and processes_per_node are not passed to submit_to_azure_if_needed, then
                 # submit_run_v2 should receive the default values of 1 and 1 respectively
                 expected_num_nodes = 1
-                expected_processes_per_node = 1
                 _ = himl.submit_to_azure_if_needed(
                     workspace_config_file="mockconfig.json",
                     snapshot_root_directory="dummy",
@@ -1804,7 +1804,7 @@ def test_submit_to_azure_v2_distributed() -> None:
                 _, call_kwargs = mock_submit_run_v2.call_args
                 print(call_kwargs)
                 assert call_kwargs.get("num_nodes") == expected_num_nodes
-                assert call_kwargs.get("processes_per_node") == expected_processes_per_node
+                assert call_kwargs.get("pytorch_processes_per_node") is None
 
                 # If num_nodes and processes_per_node are both passed in to submit_to_azure_if_needed,
                 # they should be passed through to submit_run_v2
@@ -1816,14 +1816,13 @@ def test_submit_to_azure_v2_distributed() -> None:
                     submit_to_azureml=True,
                     strictly_aml_v1=False,
                     num_nodes=num_nodes,
-                    processes_per_node_v2=processes_per_node
+                    pytorch_processes_per_node_v2=processes_per_node
                 )
                 _, call_kwargs = mock_submit_run_v2.call_args
                 assert call_kwargs.get("num_nodes") == num_nodes
-                assert call_kwargs.get("processes_per_node") == processes_per_node
+                assert call_kwargs.get("pytorch_processes_per_node") == processes_per_node
 
-            # If num_nodes and/or processes_per_node are passed in to submit_to_azure_if_needed with
-            # values less than one, they should be updated to values of 1 before the command is created
+            # Single node job: The "distribution" argument of "command" should be set to None
             with patch("health_azure.himl.command") as mock_command:
                 _ = himl.submit_to_azure_if_needed(
                     workspace_config_file="mockconfig.json",
@@ -1831,10 +1830,67 @@ def test_submit_to_azure_v2_distributed() -> None:
                     snapshot_root_directory=Path.cwd(),
                     submit_to_azureml=True,
                     strictly_aml_v1=False,
-                    num_nodes=-1,
-                    processes_per_node_v2=0
                 )
                 mock_command.assert_called_once()
                 _, call_kwargs = mock_command.call_args
                 assert call_kwargs.get("instance_count") == 1
-                assert call_kwargs.get("distribution").get("process_count_per_instance") == 1
+                assert call_kwargs.get("distribution") is None
+
+            with pytest.raises(ValueError, match="num_nodes must be >= 1"):
+                _ = himl.submit_to_azure_if_needed(
+                    workspace_config_file="mockconfig.json",
+                    entry_script=Path(__file__),
+                    snapshot_root_directory=Path.cwd(),
+                    submit_to_azureml=True,
+                    strictly_aml_v1=False,
+                    num_nodes=0
+                )
+
+            with pytest.raises(ValueError, match="pytorch_processes_per_node must be >= 1"):
+                _ = himl.submit_to_azure_if_needed(
+                    workspace_config_file="mockconfig.json",
+                    entry_script=Path(__file__),
+                    snapshot_root_directory=Path.cwd(),
+                    submit_to_azureml=True,
+                    strictly_aml_v1=False,
+                    num_nodes=1,
+                    pytorch_processes_per_node_v2=0,
+                )
+
+            # When specifying the number of pytorch processes, a PyTorchDistribution job should be created
+            num_nodes = 2
+            num_processes = 3
+            with patch("health_azure.himl.command") as mock_command:
+                _ = himl.submit_to_azure_if_needed(
+                    workspace_config_file="mockconfig.json",
+                    entry_script=Path(__file__),
+                    snapshot_root_directory=Path.cwd(),
+                    submit_to_azureml=True,
+                    strictly_aml_v1=False,
+                    num_nodes=num_nodes,
+                    pytorch_processes_per_node_v2=num_processes
+                )
+                mock_command.assert_called_once()
+                _, call_kwargs = mock_command.call_args
+                assert call_kwargs.get("instance_count") == num_nodes
+                distribution = call_kwargs.get("distribution")
+                assert isinstance(distribution, PyTorchDistribution)
+                # pytorch_processes_per_node_v2=0 should be updated to 1
+                assert distribution.process_count_per_instance == num_processes
+
+            # If num_nodes is specified without processes_per_node, an MPI job should be created.
+            with patch("health_azure.himl.command") as mock_command:
+                _ = himl.submit_to_azure_if_needed(
+                    workspace_config_file="mockconfig.json",
+                    entry_script=Path(__file__),
+                    snapshot_root_directory=Path.cwd(),
+                    submit_to_azureml=True,
+                    strictly_aml_v1=False,
+                    num_nodes=num_nodes,
+                )
+                mock_command.assert_called_once()
+                _, call_kwargs = mock_command.call_args
+                assert call_kwargs.get("instance_count") == num_nodes
+                distribution = call_kwargs.get("distribution")
+                assert isinstance(distribution, MpiDistribution)
+                assert distribution.process_count_per_instance == 1