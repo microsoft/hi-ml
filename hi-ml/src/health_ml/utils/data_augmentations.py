e64f7f52bb06ead711cd2820671515a79d13c86e
Harshita Sharma <t-hsharma@microsoft.com>
2023-01-23 18:23:56 +0000

    flake8 error

--- hi-ml/src/health_ml/utils/data_augmentations.py
+++ hi-ml/src/health_ml/utils/data_augmentations.py
@@ -2,7 +2,6 @@
 #  Copyright (c) Microsoft Corporation. All rights reserved.
 #  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 #  ------------------------------------------------------------------------------------------
-from typing import Optional
 import cv2
 import numpy as np
 import torch
