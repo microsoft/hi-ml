ebc030e5f2dc97a30a77d466778e5405dd8ef50e
Harshita Sharma <61745616+harshita-s@users.noreply.github.com>
2023-02-08 12:33:43 +0000

    ENH: Stratify PR and ROC curves by metadata (#788)
    In this PR:
    - `stratify_plots_by` parameter is introduced in BaseMIL to specify
    metadata column on which the output PR and ROC curves can be stratified
    - Functionality is added to plot the output curves stratified by
    metadata
    - Corresponding tests are added
    
    ---------
    
    Co-authored-by: Harshita Sharma <t-hsharma@microsoft.com>

--- hi-ml-cpath/src/health_cpath/configs/classification/BaseMIL.py
+++ hi-ml-cpath/src/health_cpath/configs/classification/BaseMIL.py
@@ -74,6 +74,9 @@ class BaseMIL(LightningContainer, LoadingParams, EncoderParams, PoolingParams, C
     save_intermediate_outputs: bool = param.Boolean(
         True, doc="Whether to save intermediate validation outputs during training."
     )
+    stratify_plots_by: Optional[str] = param.String(None,
+                                                    doc="Name of metadata field to stratify output plots"
+                                                    "(PR curve, ROC curve).")
 
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
@@ -148,6 +151,7 @@ class BaseMIL(LightningContainer, LoadingParams, EncoderParams, PoolingParams, C
             test_plot_options=self.get_test_plot_options(),
             loading_params=create_from_matching_params(self, LoadingParams),
             save_intermediate_outputs=self.save_intermediate_outputs,
+            stratify_plots_by=self.stratify_plots_by
         )
         if self.num_top_slides > 0:
             outputs_handler.tiles_selector = TilesSelector(
