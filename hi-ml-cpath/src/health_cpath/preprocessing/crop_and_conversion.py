4b1ce8be09630f6b007d4f2903dbfdcfd68fb0d4
Kenza Bouzid <t-kbouzid@microsoft.com>
2023-02-14 18:53:02 +0000

    wip

---
+++ hi-ml-cpath/src/health_cpath/preprocessing/crop_and_conversion.py
@@ -0,0 +1,144 @@
+from enum import Enum
+import logging
+import math
+import numpy as np
+
+from health_cpath.preprocessing.loading import WSIBackend
+from health_cpath.utils.naming import SlideKey
+from monai.data.wsi_reader import WSIReader
+from monai.transforms import MapTransform
+from openslide import OpenSlide
+from pathlib import Path
+from tifffile.tifffile import TiffWriter, PHOTOMETRIC, COMPRESSION
+from typing import Any, Dict, List, Optional
+
+
+AMPERSAND = "&"
+UNDERSCORE = "_"
+
+
+class WSIFormat(str, Enum):
+    """The format of the wsi file."""
+    NDPI = "ndpi"
+    SVS = "svs"
+    TIF = "tif"
+    TIFF = "tiff"
+
+
+class ConvertToTiffd(MapTransform):
+    """Converts a wsi file to a tiff file at a given magnification level. Additionally, the highest resolution level of
+    the wsi is also saved in the tiff file if the highest resolution level is not equal to the target resolution and
+    add_lowest_magnification is True. The tiff files are saved in the dest_dir.
+    Openslide backend is used to read the original wsi files.
+    """
+    def __init__(
+        self,
+        dest_dir: Path,
+        image_key: str = SlideKey.IMAGE,
+        src_format: WSIFormat = WSIFormat.NDPI,
+        target_magnifications: Optional[List[float]] = [10.],
+        add_lowest_magnification: bool = False,
+        replace_ampersand_by: str = AMPERSAND,
+    ) -> None:
+        self.dest_dir = dest_dir
+        self.src_format = src_format
+        self.image_key = image_key
+        self.target_magnifications = target_magnifications
+        self.wsi_reader = WSIReader(WSIBackend.OPENSLIDE)
+        self.add_lowest_magnification = add_lowest_magnification
+        self.replace_ampersand_by = replace_ampersand_by
+
+    def get_tiff_path(self, src_path: Path) -> Path:
+        tiff_filename = src_path.name.replace(self.src_format, WSIFormat.TIFF.value)
+        tiff_filename = tiff_filename.replace(AMPERSAND, self.replace_ampersand_by)
+        return self.dest_dir / tiff_filename
+
+    def _get_target_level(self, wsi_obj: OpenSlide, target_magnification: float) -> int:
+        """Returns the level of the wsi pyramid that is equal to the target magnification.
+
+        :param wsi_obj: the wsi object in openslide format
+        :param target_magnification: the target magnification e.g. 10x
+        """
+        base_objective_power = int(wsi_obj.properties['openslide.objective-power'])
+        for level_idx, level_downsample in enumerate(wsi_obj.level_downsamples):
+            objective_power = base_objective_power / level_downsample
+            if math.isclose(objective_power, target_magnification, rel_tol=1e-3):
+                return level_idx
+        raise ValueError(f"Target magnification {target_magnification} not found")
+
+    def _get_highest_level(self, wsi_obj: OpenSlide) -> int:
+        """Returns the highest resolution level of the wsi pyramid."""
+        return len(wsi_obj.level_downsamples) - 1
+
+    def _get_level_data(self, wsi_obj: OpenSlide, level: int) -> np.ndarray:
+        """Returns data from a given level of the wsi pyramid. The data is returned in HWC format as expected by the
+        tiffwriter. The data must be in HWC format otherwise the compression will not work properly.
+
+        :param wsi_obj: The wsi object in openslide format
+        :param level: The level at which the data is extracted from the wsi pyramid
+        :return: A numpy array of shape (H, W, C) where H is the height, W is the width and C is the number of channels
+        """
+        level_data, _ = self.wsi_reader.get_data(wsi_obj, level=level)
+        return level_data.transpose(1, 2, 0)
+
+    def _get_target_levels(self, wsi_obj: Any) -> List[int]:
+        """Returns the levels of the wsi pyramid that will be saved in the tiff file. If target_magnifications is not
+        None, we return the levels that correspond to the target magnifications. If add_lowest_magnification is True, we
+        add the highest resolution level of the wsi pyramid if it is not already in the target levels.
+        Otherwise, we return all levels.
+
+        :param wsi_obj: The wsi object in openslide format
+        :return: A list of levels that will be saved in the tiff file
+        """
+        if self.target_magnifications is not None:
+            target_levels = [self._get_target_level(wsi_obj, mag) for mag in self.target_magnifications]
+            if self.add_lowest_magnification:
+                highest_level = self._get_highest_level(wsi_obj)
+                if highest_level not in target_levels:
+                    target_levels.append(highest_level)
+            return target_levels
+        return [level for level in range(len(wsi_obj.level_downsamples))]
+
+    def convert_wsi(self, src_path: Path, tiff_path: Path) -> None:
+        """Converts a single wsi file from a src format to tiff format. The tiff file is saved in the tiff_path. If the
+        original src file does not have the target resolution, we skip the wsi and return None.
+
+        :param src_path: The path to the src wsi file
+        :param tiff_path: The path to the tiff file
+        """
+
+        wsi_obj = self.wsi_reader.read(src_path)
+        try:
+            levels = self._get_target_levels(wsi_obj)
+        except ValueError as e:
+            logging.warning(f"Skipping {src_path} because {e}")
+            return
+
+        resolution_unit = wsi_obj.properties['tiff.ResolutionUnit']
+        assert resolution_unit == 'centimeter', f"Resolution unit is not in centimeters: {resolution_unit}"
+        um_per_cm = 10000.
+        options = dict(
+            software='tifffile',
+            metadata={'axes': 'YXC'},
+            photometric=PHOTOMETRIC.RGB,
+            resolutionunit=resolution_unit,
+            compression=COMPRESSION.ADOBE_DEFLATE,  # ADOBE_DEFLATE aka ZLIB lossless compression
+            tile=(512, 512),  # 512x512 tiles
+        )
+        with TiffWriter(tiff_path, bigtiff=True) as tif:
+            for i, level in enumerate(levels):
+                level_data = self._get_level_data(wsi_obj, level)
+                assert level_data.shape[2] == 3, f"Expected 3 channels but got {level_data.shape[2]}. Maybe the image" \
+                                                 " is in channel first format? Try transposing the image."
+                um_per_px = self.wsi_reader.get_mpp(wsi_obj, level=level)
+                px_per_cm = (um_per_cm / um_per_px[0], um_per_cm / um_per_px[1])
+                # the subfiletype parameter is a bitfield that determines if the wsi_level is a reduced version of
+                # another image. level 0 (i.e. i=0) is the full resolution image in the pyramid.
+                tif.write(level_data, resolution=px_per_cm, subfiletype=int(i > 0), **options)
+
+    def __call__(self, data: Dict) -> Dict:
+        src_path = Path(data[self.image_key])
+        tiff_path = self.get_tiff_path(src_path)
+        if not tiff_path.exists():
+            self.convert_wsi(src_path, tiff_path)
+        return data
---
+++ hi-ml-cpath/src/health_cpath/utils/crop_conversion_config.py
@@ -0,0 +1,95 @@
+
+#  -------------------------------------------------------------------------------------------
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+#  -------------------------------------------------------------------------------------------
+from argparse import ArgumentParser
+import logging
+import sys
+import param
+from copy import deepcopy
+from health_azure.logging import logging_section
+from health_azure.utils import apply_overrides, create_argparser, parse_arguments
+from health_cpath.datasets.base_dataset import SlidesDataset
+from health_cpath.preprocessing.crop_and_conversion import AMPERSAND, UNDERSCORE, ConvertToTiffd, WSIFormat
+from health_cpath.utils.montage_config import AzureRunConfig
+from health_cpath.utils.naming import SlideKey
+from monai.data.dataset import Dataset
+from pathlib import Path
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from typing import Any, List, Optional
+
+
+class TiffConversionConfig(AzureRunConfig):
+    dest_dir: Path = param.ClassSelector(
+        class_=Path, default=Path("outputs"), doc="The folder where the new tiff files will be saved."
+    )
+    src_format: WSIFormat = param.ClassSelector(
+        class_=WSIFormat, default=WSIFormat.NDPI, doc="The format of the source files. Default is NDPI."
+    )
+    image_key: str = param.String(
+        default=SlideKey.IMAGE,
+        doc="The key of the image in the dataset. This is used to get the path of the src file."
+    )
+    target_magnifications: Optional[List[float]] = param.List(
+        default=[10.],
+        doc="The magnifications that will be saved in the tiff files. Use None for all available magnifications."
+    )
+    add_lowest_magnification: bool = param.Boolean(
+        default=False,
+        doc="If True, the lowest magnification will be saved in the tiff files additionally. If False, only the "
+            "specified magnifications will be saved. This is especially useful for costly computations that can be "
+            "applied at a lower magnification."
+    )
+    replace_ampersand_by: str = param.String(
+        default=UNDERSCORE,
+        doc="The character that will replace the ampersand in the file name. & (as in H&E) can be problematic in some "
+            "file systems. It is recommended to use _ instead."
+    )
+    num_workers: int = param.Integer(
+        default=1,
+        doc="The number of workers that will be used to convert the src files to tiff files. If num_workers is 1."
+    )
+
+    def __init__(self, dataset: SlidesDataset, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.transform = ConvertToTiffd(
+            dest_dir=self.dest_dir,
+            image_key=self.image_key,
+            src_format=self.src_format,
+            target_magnifications=self.target_magnifications,
+            add_lowest_magnification=self.add_lowest_magnification,
+            replace_ampersand_by=self.replace_ampersand_by,
+        )
+        self.dataset = dataset
+
+    def run_conversion(self) -> None:
+        transformed_dataset = Dataset(self.dataset, self.transform)  # type: ignore
+        dataloader = DataLoader(transformed_dataset, num_workers=self.num_workers, batch_size=1)
+        with logging_section(f"Starting conversion of {len(self.dataset)} slides to tiff format to {self.dest_dir}"):
+            for _ in tqdm(dataloader, total=len(dataloader)):
+                pass
+
+    def create_dataset_csv_for_transformed_data(self, dest_file_name: Optional[str]) -> None:
+        new_dataset_df = deepcopy(self.dataset.dataset_df)
+        new_dataset_df[self.dataset.IMAGE_COLUMN] = new_dataset_df[self.dataset.IMAGE_COLUMN].str.replace(
+            self.src_format, WSIFormat.TIFF).str.replace(AMPERSAND, self.replace_ampersand_by)
+        new_dataset_path = self.dest_dir / (dest_file_name or self.dataset.DEFAULT_CSV_FILENAME)
+        new_dataset_df.to_csv(new_dataset_path, sep="\t" if new_dataset_path.suffix == ".tsv" else ",")
+        logging.info(f"Saved new dataset tsv file to {new_dataset_path}")
+
+
+def create_tiff_conversion_argparser(dataset: SlidesDataset) -> ArgumentParser:
+    return create_argparser(
+        TiffConversionConfig(dataset),
+        usage="python convert_to_tiff.py --dataset <dataset_folder> --image_glob_pattern '**/*.tiff' --width 1000",
+        description="Create an overview image with thumbnails of all slides in a dataset.")
+
+
+def create_tiff_conversion_config_from_args(dataset: SlidesDataset) -> TiffConversionConfig:
+    parser = create_tiff_conversion_argparser(dataset)
+    config = TiffConversionConfig()
+    parser_results = parse_arguments(parser, args=sys.argv[1:], fail_on_unknown_args=True)
+    _ = apply_overrides(config, parser_results.args)
+    return config
