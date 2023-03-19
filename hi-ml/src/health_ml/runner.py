ef927917a7210bef3d6c98846ae234bb65e0daa2
Anton Schwaighofer <antonsc@microsoft.com>
2023-03-19 14:27:25 +0000

    use local rank

--- hi-ml/src/health_ml/runner.py
+++ hi-ml/src/health_ml/runner.py
@@ -32,7 +32,7 @@ from health_azure.datasets import create_dataset_configs  # noqa: E402
 from health_azure.himl import DEFAULT_DOCKER_BASE_IMAGE, OUTPUT_FOLDER  # noqa: E402
 from health_azure.logging import logging_to_stdout   # noqa: E402
 from health_azure.paths import is_himl_used_from_git_repo  # noqa: E402
-from health_azure.utils import (ENV_GLOBAL_RANK, get_workspace, get_ml_client, is_local_rank_zero,  # noqa: E402
+from health_azure.utils import (ENV_LOCAL_RANK, get_workspace, get_ml_client, is_local_rank_zero,  # noqa: E402
                                 is_running_in_azure_ml, set_environment_variables_for_multi_node,
                                 create_argparser, parse_arguments, ParserResult, apply_overrides,
                                 filter_v2_input_output_args, is_global_rank_zero)
@@ -335,8 +335,7 @@ def run(project_root: Path) -> Tuple[LightningContainer, AzureRunInfo]:
     if is_running_in_azure_ml():
         # Create a timestamped filename. This will also ensure that all restarts after low-priority preemption create
         # a new log file, and we can fully trace back what happened in each rank in each restart.
-        rank = os.getenv(ENV_GLOBAL_RANK, "0")
-
+        rank = os.getenv(ENV_LOCAL_RANK, "0")
         timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H%M%S")
         cwd = Path.cwd()
         logging_filename = cwd
