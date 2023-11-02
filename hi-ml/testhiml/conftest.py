9cc6706e748e2a34dc894df89aa0dc80848a3994
Anton Schwaighofer <antonsc@microsoft.com>
2023-11-02 04:13:34 -0700

    Revert "test dir"
    This reverts commit 6c5c8a6b036ffe2d529da8ce00dfdfbbc76865ce.

--- hi-ml/testhiml/conftest.py
+++ hi-ml/testhiml/conftest.py
@@ -1,23 +1,11 @@
 from pathlib import Path
-import uuid
 import pytest
 
 from health_ml.runner import Runner
 from health_ml.utils import health_ml_package_setup
-from health_ml.utils.fixed_paths import repository_root_directory
 
 # Reduce logging noise in DEBUG mode
 health_ml_package_setup()
-
-root = repository_root_directory()
-
-print("Creating test outputs folder.")
-new_dir = root / ("test_outputs_" + uuid.uuid4().hex)
-new_dir.mkdir()
-assert new_dir.is_dir(), "New directory should have been created."
-new_file = new_dir / "test_file.txt"
-new_file.write_text("Hello world!")
-assert new_file.is_file(), "New file should have been created."
 
 
 @pytest.fixture
