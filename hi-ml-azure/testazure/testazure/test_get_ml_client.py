1a0226dbb9e8abf02ba00eb61ab2147a21de009a
Anton Schwaighofer <antonsc@microsoft.com>
2024-03-19 08:38:10 +0000

    fix fast tests

--- hi-ml-azure/testazure/testazure/test_get_ml_client.py
+++ hi-ml-azure/testazure/testazure/test_get_ml_client.py
@@ -42,73 +42,69 @@ def test_get_credential() -> None:
         ENV_SERVICE_PRINCIPAL_PASSWORD: "baz",
     }
 
-    with patch.dict(os.environ, mock_env_vars):
-        with patch.multiple(
-            "health_azure.utils",
-            is_running_in_azure_ml=DEFAULT,
-            is_running_on_azure_agent=DEFAULT,
-            _get_legitimate_service_principal_credential=DEFAULT,
-            _get_legitimate_device_code_credential=DEFAULT,
-            _get_legitimate_default_credential=DEFAULT,
-            _get_legitimate_interactive_browser_credential=DEFAULT,
-        ) as mocks:
-            mocks["is_running_in_azure_ml"].return_value = False
-            mocks["is_running_on_azure_agent"].return_value = False
-            _ = get_credential()
-            mocks["_get_legitimate_service_principal_credential"].assert_called_once()
-            mocks["_get_legitimate_device_code_credential"].assert_not_called()
-            mocks["_get_legitimate_default_credential"].assert_not_called()
-            mocks["_get_legitimate_interactive_browser_credential"].assert_not_called()
+    with patch.multiple(
+        "health_azure.utils",
+        is_running_in_azure_ml=MagicMock(return_value=False),
+        is_running_on_azure_agent=MagicMock(return_value=False),
+    ):
+        with patch.dict(os.environ, mock_env_vars):
+            with patch.multiple(
+                "health_azure.auth",
+                _get_legitimate_service_principal_credential=DEFAULT,
+                _get_legitimate_device_code_credential=DEFAULT,
+                _get_legitimate_default_credential=DEFAULT,
+                _get_legitimate_interactive_browser_credential=DEFAULT,
+            ) as mocks:
+                _ = get_credential()
+                mocks["_get_legitimate_service_principal_credential"].assert_called_once()
+                mocks["_get_legitimate_device_code_credential"].assert_not_called()
+                mocks["_get_legitimate_default_credential"].assert_not_called()
+                mocks["_get_legitimate_interactive_browser_credential"].assert_not_called()
 
-    # if the environment variables are not set and we are running on a local machine, a
-    # DefaultAzureCredential should be attempted first
-    with patch.object(os.environ, "get", return_value={}):
-        with patch.multiple(
-            "health_azure.utils",
-            is_running_in_azure_ml=DEFAULT,
-            is_running_on_azure_agent=DEFAULT,
-            _get_legitimate_service_principal_credential=DEFAULT,
-            _get_legitimate_device_code_credential=DEFAULT,
-            _get_legitimate_default_credential=DEFAULT,
-            _get_legitimate_interactive_browser_credential=DEFAULT,
-        ) as mocks:
-            mock_get_sp_cred = mocks["_get_legitimate_service_principal_credential"]
-            mock_get_device_cred = mocks["_get_legitimate_device_code_credential"]
-            mock_get_default_cred = mocks["_get_legitimate_default_credential"]
-            mock_get_browser_cred = mocks["_get_legitimate_interactive_browser_credential"]
+        # if the environment variables are not set and we are running on a local machine, a
+        # DefaultAzureCredential should be attempted first
+        with patch.object(os.environ, "get", return_value={}):
+            with patch.multiple(
+                "health_azure.auth",
+                _get_legitimate_service_principal_credential=DEFAULT,
+                _get_legitimate_device_code_credential=DEFAULT,
+                _get_legitimate_default_credential=DEFAULT,
+                _get_legitimate_interactive_browser_credential=DEFAULT,
+            ) as mocks:
+                mock_get_sp_cred = mocks["_get_legitimate_service_principal_credential"]
+                mock_get_device_cred = mocks["_get_legitimate_device_code_credential"]
+                mock_get_default_cred = mocks["_get_legitimate_default_credential"]
+                mock_get_browser_cred = mocks["_get_legitimate_interactive_browser_credential"]
+                _ = get_credential()
+                mock_get_sp_cred.assert_not_called()
+                mock_get_device_cred.assert_not_called()
+                mock_get_default_cred.assert_called_once()
+                mock_get_browser_cred.assert_not_called()
 
-            mocks["is_running_in_azure_ml"].return_value = False
-            mocks["is_running_on_azure_agent"].return_value = False
-            _ = get_credential()
-            mock_get_sp_cred.assert_not_called()
-            mock_get_device_cred.assert_not_called()
-            mock_get_default_cred.assert_called_once()
-            mock_get_browser_cred.assert_not_called()
+                # if that fails, a DeviceCode credential should be attempted
+                mock_get_default_cred.side_effect = _mock_validation_error
+                _ = get_credential()
+                mock_get_sp_cred.assert_not_called()
+                mock_get_device_cred.assert_called_once()
+                assert mock_get_default_cred.call_count == 2
+                mock_get_browser_cred.assert_not_called()
 
-            # if that fails, a DeviceCode credential should be attempted
-            mock_get_default_cred.side_effect = _mock_validation_error
-            _ = get_credential()
-            mock_get_sp_cred.assert_not_called()
-            mock_get_device_cred.assert_called_once()
-            assert mock_get_default_cred.call_count == 2
-            mock_get_browser_cred.assert_not_called()
+                # if None of the previous credentials work, an InteractiveBrowser credential should be tried
+                mock_get_device_cred.return_value = None
+                _ = get_credential()
+                mock_get_sp_cred.assert_not_called()
+                assert mock_get_device_cred.call_count == 2
+                assert mock_get_default_cred.call_count == 3
+                mock_get_browser_cred.assert_called_once()
 
-            # if None of the previous credentials work, an InteractiveBrowser credential should be tried
-            mock_get_device_cred.return_value = None
-            _ = get_credential()
-            mock_get_sp_cred.assert_not_called()
-            assert mock_get_device_cred.call_count == 2
-            assert mock_get_default_cred.call_count == 3
-            mock_get_browser_cred.assert_called_once()
-
-            # finally, if none of the methods work, an Exception should be raised
-            mock_get_browser_cred.return_value = None
-            with pytest.raises(Exception) as e:
-                get_credential()
-                assert (
-                    "Unable to generate and validate a credential. Please see Azure ML documentation"
-                    "for instructions on different options to get a credential" in str(e)
-                )
+                # finally, if none of the methods work, an Exception should be raised
+                mock_get_browser_cred.return_value = None
+                with pytest.raises(Exception) as e:
+                    get_credential()
+                    assert (
+                        "Unable to generate and validate a credential. Please see Azure ML documentation"
+                        "for instructions on different options to get a credential" in str(e)
+                    )
 
 
 @pytest.mark.fast
@@ -127,7 +123,7 @@ def test_get_legitimate_service_principal_credential() -> None:
         assert expected_error_msg in str(e)
 
     # now mock the case where validating the credential succeeds and check the value of that
-    with patch("health_azure.utils._validate_credential"):
+    with patch("health_azure.auth._validate_credential"):
         cred = _get_legitimate_service_principal_credential(
             mock_tenant_id, mock_service_principal_id, mock_service_principal_password
         )
@@ -139,12 +135,12 @@ def test_get_legitimate_device_code_credential() -> None:
     def _mock_credential_fast_timeout(timeout: int) -> DeviceCodeCredential:
         return DeviceCodeCredential(timeout=1)
 
-    with patch("health_azure.utils.DeviceCodeCredential", new=_mock_credential_fast_timeout):
+    with patch("health_azure.auth.DeviceCodeCredential", new=_mock_credential_fast_timeout):
         cred = _get_legitimate_device_code_credential()
         assert cred is None
 
     # now mock the case where validating the credential succeeds
-    with patch("health_azure.utils._validate_credential"):
+    with patch("health_azure.auth._validate_credential"):
         cred = _get_legitimate_device_code_credential()
         assert isinstance(cred, DeviceCodeCredential)
 
@@ -154,12 +150,12 @@ def test_get_legitimate_default_credential() -> None:
     def _mock_credential_fast_timeout(timeout: int) -> DefaultAzureCredential:
         return DefaultAzureCredential(timeout=1)
 
-    with patch("health_azure.utils.DefaultAzureCredential", new=_mock_credential_fast_timeout):
+    with patch("health_azure.auth.DefaultAzureCredential", new=_mock_credential_fast_timeout):
         exception_message = r"DefaultAzureCredential failed to retrieve a token from the included credentials."
         with pytest.raises(ClientAuthenticationError, match=exception_message):
             cred = _get_legitimate_default_credential()
 
-    with patch("health_azure.utils._validate_credential"):
+    with patch("health_azure.auth._validate_credential"):
         cred = _get_legitimate_default_credential()
         assert isinstance(cred, DefaultAzureCredential)
 
