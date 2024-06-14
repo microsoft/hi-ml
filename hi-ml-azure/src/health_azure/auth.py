af3d8279dcd42b6690ff205f11b0a94064d61c61
Anton Schwaighofer <antonsc@microsoft.com>
2024-06-14 07:08:29 -0700

    warnings

--- hi-ml-azure/src/health_azure/auth.py
+++ hi-ml-azure/src/health_azure/auth.py
@@ -63,6 +63,10 @@ def get_authentication() -> (
     service_principal_password = get_secret_from_environment(ENV_SERVICE_PRINCIPAL_PASSWORD, allow_missing=True)
     # Check if all 3 environment variables are set
     if service_principal_id and tenant_id and service_principal_password:
+        logger.warning(
+            "The use of Service Principal credentials is discouraged because of the risk of password "
+            "compromise. Consider switching to OpenID Connect authentication."
+        )
         logger.info(
             "Found environment variables for Service Principal authentication: First characters of App ID "
             f"are {service_principal_id[:8]}... in tenant {tenant_id[:8]}..."
@@ -189,6 +193,10 @@ def get_credential() -> Optional[TokenCredential]:
     tenant_id = get_secret_from_environment(ENV_TENANT_ID, allow_missing=True)
     service_principal_password = get_secret_from_environment(ENV_SERVICE_PRINCIPAL_PASSWORD, allow_missing=True)
     if service_principal_id and tenant_id and service_principal_password:
+        logger.warning(
+            "The use of Service Principal credentials is discouraged because of the risk of password "
+            "compromise. Consider switching to OpenID Connect authentication."
+        )
         logger.info(
             "Found environment variables for Service Principal authentication: First characters of App ID "
             f"are {service_principal_id[:8]}... in tenant {tenant_id[:8]}..."
